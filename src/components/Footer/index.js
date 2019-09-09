6f7855e3d665c635b10947d251a377527987ba89
Timur Çelikel <me@timurcelikel.com>
2019-09-09 16:14:16 +0200

    refactor: move footer to separate folder

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import style from './style.module.css'
+
+export default () => (
+  <footer className={style.siteFooter}>
+    <div className={style.siteFooterContent}>
+      <section className="copyright">
+        &copy; Grundeinkommensbüro 2019 | <Link to="/privacy">Datenschutz</Link>{' '}
+        | <Link to="/imprint">Impressum</Link>
+      </section>
+    </div>
+  </footer>
+)
