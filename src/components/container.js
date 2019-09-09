019e02540704b853a584b712d202c50e5135e5e4
Timur Çelikel <me@timurcelikel.com>
2019-09-09 16:14:04 +0200

    refactor: remove "container"

--- src/components/container.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Footer from './footer';
-
-export default ({ children }) => (
-  <div>
-  <div style={{ maxWidth: 1180, margin: '0 auto' }}>{children}</div>
-  <Footer />
-  </div>
-)
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,24 +1,17 @@
 import React from 'react'
-import { Link } from 'gatsby'
-import base from './base.css'
-import Container from './container'
 import Navigation from './navigation'
+import Footer from './Footer'
 
 class Template extends React.Component {
   render() {
-    const { location, children } = this.props
-    let header
-
-    let rootPath = `/`
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`
-    }
+    const { children } = this.props
 
     return (
-      <Container>
+      <div style={{ maxWidth: 1180, margin: '0 auto' }}>
         <Navigation />
         {children}
-      </Container>
+        <Footer />
+      </div>
     )
   }
 }
