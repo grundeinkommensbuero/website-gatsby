4f0089eb2a218d5dfec75a7845112c892457deb1
Timur Çelikel <me@timurcelikel.com>
2019-09-09 16:22:42 +0200

    fix jxs property

--- src/components/email-list-form.js
+++ src/components/email-list-form.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
-import styles from './email-list-form.module.css';
+import React, { useState } from 'react'
+import styles from './email-list-form.module.css'
 
 function EmailListForm() {
-
   return (
-    <div class="ml-form-embed"
+    <div
+      className="ml-form-embed"
       data-account="1629538:k8n3g9j9x8"
-      data-form="1447292:a4q8c2">
-    </div>
+      data-form="1447292:a4q8c2"
+    ></div>
+  )
+}
 
-  );
-};
-
-export default EmailListForm;
+export default EmailListForm
