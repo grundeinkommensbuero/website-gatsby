5f246fbc553ab465967626e9eeaf69c335e26ef4
Timur Çelikel <me@timurcelikel.com>
2019-09-10 18:26:45 +0200

    landing page: replace current stuff with more flexible sections

---
+++ src/components/Sections/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import s from './style.module.less'
+
+export default function Sections({ sections }) {
+  if (sections && sections.length) {
+    return (
+      <div>
+        {sections.map((section, index) => (
+          <section key={index}>
+            {section.title && <h1>{section.title}</h1>}
+            {documentToReactComponents(section.body.json)}
+          </section>
+        ))}
+      </div>
+    )
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,76 +6,16 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import MailerLite from '../components/MailerLite'
 import EmailListForm from '../components/EmailListForm'
+import Sections from '../components/Sections'
 
 class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const [content] = get(this, 'props.data.allContentfulLandingPage.edges')
+    const sections = get(content, 'node.sections')
     return (
       <Layout location={this.props.location}>
-        <MailerLite />
-        <div>
-          <Helmet title={siteTitle} />
-          {/* <Hero data={content.node} /> */}
-          <div className="wrapper">
-            <h2 className="section-headline" id="anliegen">
-              Unser Anliegen
-            </h2>
-            <p>{content.node.anliegen.anliegen}</p>
-          </div>
-        </div>
-        <div className="section-gray">
-          <div className="wrapper">
-            <h2 className="section-headline" id="vorhaben">
-              Wie machen wir das?
-            </h2>
-            <p>{content.node.vorhaben.vorhaben}</p>
-          </div>
-        </div>
-        <div>
-          <div className="wrapper">
-            <h2 className="section-headline" id="newsletter">
-              Bist du dabei? Dann trage dich in unser Newsletter ein!
-            </h2>
-            <p>
-              Trage dich in unsere Mailingliste ein und erhalte regelmäßig alle
-              Neuigkeiten zum Projekt.
-            </p>
-            <EmailListForm />
-          </div>
-        </div>
-        <div className="section-gray">
-          <div className="wrapper">
-            <ul className="article-list">
-              <li key="map">
-                <a
-                  target="_blank"
-                  href="https://www.openstreetmap.org/?mlat=52.4831&mlon=13.4310#map=15/52.4831/13.4310&layers=H"
-                >
-                  <img alt="Bildbeschreibungs-Text" src="geb.png" border="0" />
-                </a>
-              </li>
-              <li key="sumary">
-                <p>
-                  Fragen? Antworten? Schreib uns! <br />{' '}
-                  info@grundeinkommensbüro.de <br />
-                  <br />
-                  <br />
-                  Grundeinkommensbüro
-                  <br />
-                  Karl-Marx-Strasse 50
-                  <br />
-                  12043 Berlin
-                </p>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          <div>
-            <h2 className="section-headline"></h2>
-          </div>
-        </div>
+        <Sections sections={sections} />
       </Layout>
     )
   }
@@ -96,21 +36,12 @@ export const pageQuery = graphql`
       edges {
         node {
           title
-          vorhaben {
-            vorhaben
-          }
-          anliegen {
-            anliegen
-          }
-          heroImage: image {
-            fluid(
-              maxWidth: 1180
-              maxHeight: 480
-              resizingBehavior: PAD
-              background: "rgb:000000"
-            ) {
-              ...GatsbyContentfulFluid_tracedSVG
+          sections {
+            title
+            body {
+              json
             }
+            emailSignup
           }
         }
       }
