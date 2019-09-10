f9c8031900d6a0abfec7e1f799adef201f2a80ba
Timur Çelikel <me@timurcelikel.com>
2019-09-10 22:11:04 +0200

    fix mail list signup

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import s from './style.module.less';
+import EmailListForm from '../EmailListForm';
 
 export default function Sections({ sections }) {
   if (sections && sections.length) {
@@ -10,6 +11,7 @@ export default function Sections({ sections }) {
           <section key={index}>
             {section.title && <h1>{section.title}</h1>}
             {documentToReactComponents(section.body.json)}
+            {section.emailSignup && <EmailListForm />}
           </section>
         ))}
       </div>
