9380d110a702895e8ed1fe8ed14c665c19258b1c
Timur Çelikel <me@timurcelikel.com>
2019-09-11 16:07:20 +0200

    add favicon

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,6 +4,7 @@ import Footer from './Footer';
 import s from './style.module.less';
 import '../style/base.less';
 import Sections from '../Sections';
+import Helmet from 'react-helmet';
 
 class Template extends React.Component {
   render() {
@@ -12,6 +13,9 @@ class Template extends React.Component {
     return (
       <>
         <Header sections={sections} />
+        <Helmet>
+          <link rel="icon" type="image/png" href="favicon.png" />
+        </Helmet>
         <main className={s.main}>
           {children}
           <Sections sections={sections} />
