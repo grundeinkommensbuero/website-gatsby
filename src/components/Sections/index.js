f3b28a38e5179a224cdf8ea531d0c1210942d463
Timur Çelikel <me@timurcelikel.com>
2019-09-11 16:35:37 +0200

    basic section style

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -7,19 +7,21 @@ import { stringToId } from '../utils';
 export default function Sections({ sections }) {
   if (sections && sections.length) {
     return (
-      <>
+      <div className={s.sections}>
         {sections.map((section, index) => {
           const { title, titleShort, body, emailSignup } = section;
           const id = stringToId(titleShort);
           return (
-            <section key={index} id={id}>
-              {title && <h1>{title}</h1>}
-              {documentToReactComponents(body.json)}
-              {emailSignup && <EmailListForm />}
+            <section key={index} id={id} className={s.section}>
+              <div className={s.sectionBody}>
+                {title && <h1 className={s.title}>{title}</h1>}
+                {documentToReactComponents(body.json)}
+                {emailSignup && <EmailListForm />}
+              </div>
             </section>
           );
         })}
-      </>
+      </div>
     );
   }
   return null;
