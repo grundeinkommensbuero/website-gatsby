81bc358896967a800c702c3b9faa1abbdb56fef2
Timur Çelikel <me@timurcelikel.com>
2019-09-12 11:12:26 +0200

    generate static pages dynamically

---
+++ src/components/StaticPage/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import get from 'lodash/get';
+import Layout from '../Layout';
+import Helmet from 'react-helmet';
+
+class StaticPage extends React.Component {
+  render() {
+    const page = get(this.props, 'data.contentfulStaticContent');
+
+    console.log(page);
+
+    return (
+      <Layout location={this.props.location} title={page.title}>
+        <Helmet>
+          <title>{page.title}</title>
+
+          {page.description && (
+            <meta
+              name="description"
+              content={page.description.internal.content}
+            />
+          )}
+        </Helmet>
+        <h1>{page.title}</h1>
+        <p>{page.publishDate}</p>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: page.body.childMarkdownRemark.html,
+          }}
+        />
+      </Layout>
+    );
+  }
+}
+
+export default StaticPage;
+
+export const pageQuery = graphql`
+  query StaticPageBySlug($slug: String!) {
+    contentfulStaticContent(slug: { eq: $slug }) {
+      title
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+      description {
+        internal {
+          content
+        }
+      }
+    }
+  }
+`;
