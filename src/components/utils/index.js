6ac7a9d21901a2ddd02eca58df6c2daf3228564a
Valioesi <Vali_Schagerl@web.de>
2019-09-23 15:11:38 +0200

    Added basic AWS cognito logic

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -2,3 +2,14 @@
 export function stringToId(string) {
   return string && string.toString().replace(/^[^a-z]+|[^\w:.-]+/gi, '');
 }
+
+// Generates a random string (e.g. for generating random password)
+export function getRandomString(bytes) {
+  const randomValues = new Uint8Array(bytes);
+  window.crypto.getRandomValues(randomValues);
+  return Array.from(randomValues).map(intToHex).join('');
+}
+
+function intToHex(nr) {
+  return nr.toString(16).padStart(2, '0');
+}
