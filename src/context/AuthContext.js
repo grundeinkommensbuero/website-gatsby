aacedcee2959c035c9d3158e9fd60f9e65d47ae6
Valioesi <Vali_Schagerl@web.de>
2019-09-26 13:58:52 +0200

    Added setting cognito attributes

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Auth } from 'aws-amplify';
 
 /**
  * This class serves as a provider (reacts context API) which is used
  * to maintain a global state. The application is wrapped around such a provider.
  * https://www.gatsbyjs.org/blog/2019-01-31-using-react-context-api-with-gatsby/
+ * 
+ * Another option would be to make use of Amplifys Auth functions, but I think that
+ * would be less efficient than keeping an aditional local state
  */
 
 const AuthContext = React.createContext();
@@ -21,6 +25,32 @@ class AuthProvider extends Component {
 
   setUser = (user) => {
     this.setState = { user: user };
+  }
+
+  setIban = async (iban) => {
+    if (user !== null) {
+      try {
+        await Auth.updateUserAttributes(this.state.user, { iban });
+        //save the changed user in the global state
+        const updatedUser = await Auth.currentAuthenticatedUser();
+        this.setState({ user: updatedUser });
+      } catch (error) {
+        console.log('Error while updating iban');
+      }
+    }
+  }
+
+  setIsDonationActive = async (isDonationActive) => {
+    if (user !== null) {
+      try {
+        await Auth.updateUserAttributes(this.state.user, { isDonationActive });
+        //save the changed user in the global state
+        const updatedUser = await Auth.currentAuthenticatedUser();
+        this.setState({ user: updatedUser });
+      } catch (error) {
+        console.log('Error while updating if donation is active');
+      }
+    }
   }
 
   render() {
