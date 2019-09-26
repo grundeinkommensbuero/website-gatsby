5a571ce20b6767b68356b7cde81484389bc3dbd8
Valioesi <Vali_Schagerl@web.de>
2019-09-26 13:21:44 +0200

    added config data for cognito + some bug fixes

--- src/components/Auth/Login/index.js
+++ src/components/Auth/Login/index.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import Auth from 'aws-amplify';
-import { getRandomString } from '../../utils'
+import { Auth } from 'aws-amplify';
+import { getRandomString } from '../../utils';
+
 
 class Login extends Component {
+
+  // This functions calls signUp (which creates the user in aws user pool) 
+  // and signIn (which starts the custom flow of sending the magic code to the mail address)
+  startSignInProcess = async () => {
+    const mail = "example@mail.com";
+    await this.signUp(mail);
+    await this.signIn(mail);
+  }
 
   // Function to sign up user through AWS Cognito 
   // Tutorial: https://aws.amazon.com/de/blogs/mobile/implementing-passwordless-email-authentication-with-amazon-cognito/
   signUp = async (email) => {
     try {
       // We have to “generate” a password for them, because a password is required by Amazon Cognito when users sign up
-      await Auth.signUp({
+      console.log(await Auth.signUp({
         username: email,
-        password: getRandomString(30)
-      });
+        password: getRandomString(30),
+        attributes: {
+          name: "testperson2"
+          //Custom attribute: iban
+          //Custom attribute: isDonationActive
+        }
+      }));
     } catch (error) {
       //TODO: Error handling in UI?
       console.log('Error while signing up', error);
@@ -20,11 +34,14 @@ class Login extends Component {
   }
 
   // Sign in user through AWS Cognito (passwordless)
-  singIn = async (email) => {
+  signIn = async (email) => {
     try {
       // This will initiate the custom flow, which will lead to the user receiving a mail.
       // The code will timeout after 3 minutes (enforced server side by AWS Cognito). 
-      await Auth.signIn(email);
+      const user = await Auth.signIn(email);
+      console.log('called Auth.signIn()');
+      //we already set the user here, because we need the object in answerCustomChallenge()
+      this.props.setUser(user);
     } catch (error) {
       //TODO: Error handling in UI?
       console.log('Error while signing in', error);
@@ -35,8 +52,8 @@ class Login extends Component {
     // Send the answer to the User Pool
     try {
       // sendCustomChallengeAnswer() will throw an error if it’s the 3rd wrong answer
-      const user = await Auth.sendCustomChallengeAnswer(cognitoUser, answer);
-
+      const user = await Auth.sendCustomChallengeAnswer(this.props.state.user, answer);
+      console.log('user after sending challenge', user);
       // It we get here, the answer was sent successfully,
       // but it might have been wrong (1st or 2nd time)
       // So we should test if the user is authenticated now
@@ -47,6 +64,7 @@ class Login extends Component {
         //a function to set the user somewhere else (e.g. global state via context api)
         this.props.setUser(user);
         this.props.setIsAuthenticated(true);
+
       } catch (error) {
         //TODO: Error handling in UI: wrong code
         console.log('Apparently the user did not enter the right code', error);
@@ -71,9 +89,14 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
-
-      </div>
+   /*   
+      If we do the container-presenter (or controller-view if you will) model, this could look
+      something like this...
+      <LoginView
+        sendCode={() => this.answerCustomChallenge(code)}
+        signIn={() => this.startSignInProcess()}
+      /> */
+      <div></div>
     );
   }
 }
