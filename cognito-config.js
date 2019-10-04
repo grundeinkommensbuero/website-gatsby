10042f308897556143cd21c01225ecf1397d468b
Valioesi <Vali_Schagerl@web.de>
2019-10-04 10:37:07 +0200

    moved cognito app client id to env

--- cognito-config.js
+++ cognito-config.js
@@ -1,5 +1,4 @@
 export const cognitoConfig = {
-  "REGION": "eu-central-1",
-  "USER_POOL_ID": "eu-central-1_gXOpHFeew",
-  "APP_CLIENT_ID": "5rl8uk580bfdnmb37sik8e18gd"
-}
+  REGION: 'eu-central-1',
+  USER_POOL_ID: 'eu-central-1_gXOpHFeew',
+};
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,16 @@
-import React from "react";
+import React from 'react';
 import Amplify from 'aws-amplify';
 import { cognitoConfig } from './cognito-config';
 import { AuthProvider } from './src/context/AuthContext';
 
-
 // This is used to wrap the page, so we can configure AWS Cognito in this wrapper,
 // so it only gets configured once, not every time a page changes
 export const wrapRootElement = ({ element }) => {
-
   Amplify.configure({
     region: cognitoConfig.REGION,
     userPoolId: cognitoConfig.USER_POOL_ID,
-    userPoolWebClientId: cognitoConfig.APP_CLIENT_ID,
+    userPoolWebClientId: process.env.COGNITO_APP_CLIENT_ID,
   });
 
-  return (
-    <AuthProvider>{element}</AuthProvider>
-  )
-}
+  return <AuthProvider>{element}</AuthProvider>;
+};
