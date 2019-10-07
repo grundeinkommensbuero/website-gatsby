4fb4e5e83fbfe583b3c26f1d729c645482a3c9e5
Timur Çelikel <me@timurcelikel.com>
2019-10-07 13:54:48 +0200

    youtube: dont show unrelated videos in the end

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -104,7 +104,7 @@ function YoutubeEmbed({ url }) {
       <iframe
         width="560"
         height="315"
-        src={`https://www.youtube.com/embed/${url}`}
+        src={`https://www.youtube.com/embed/${url}?rel=0`}
         frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
