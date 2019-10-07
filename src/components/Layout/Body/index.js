e425ca7bc2a7b7ff0354a222a65a8018e84ab542
Timur Çelikel <me@timurcelikel.com>
2019-10-07 15:25:59 +0200

    Body: remove unused component

--- src/components/Layout/Body/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from './style.module.less';
-
-export default ({ children }) => (
-  <div className={s.body}>
-    <div className={s.bodyInner}>{children}</div>
-  </div>
-);
