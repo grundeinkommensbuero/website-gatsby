7cdd9fed89ff43b91d74cf01bbf274c7156c9dbd
Valentin Schagerl <Vali_Schagerl@web.de>
2019-10-08 17:44:48 +0200

    Hotfix: Comment out configuring Cognito

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,11 +6,11 @@ import { AuthProvider } from './src/context/Authentication';
 // This is used to wrap the page, so we can configure AWS Cognito in this wrapper,
 // so it only gets configured once, not every time a page changes
 export const wrapRootElement = ({ element }) => {
-  Amplify.configure({
+ /* Amplify.configure({
     region: cognitoConfig.REGION,
     userPoolId: cognitoConfig.USER_POOL_ID,
     userPoolWebClientId: process.env.COGNITO_APP_CLIENT_ID,
   });
-
+*/
   return <AuthProvider>{element}</AuthProvider>;
 };
