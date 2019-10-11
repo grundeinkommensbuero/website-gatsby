179378587267ccf0f97271411e41c20f7c614081
Johann Barbie <johannbarbie@me.com>
2019-10-11 10:00:39 +0200

    dumb

--- src/components/AboutUs/index.js
+++ src/components/AboutUs/index.js
@@ -7,6 +7,7 @@ const AboutUs = ({ members, className }) => {
     <ul className={cN(s.aboutUs, className)}>
       {members.map((member, index) => {
         const { name, image } = member;
+        console.log('here');
         return (
           <li key={index} className={s.member}>
             {image && (
