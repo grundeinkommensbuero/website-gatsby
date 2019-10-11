06c5d884c79a91589440fcb735b3f54972a0b884
Valioesi <Vali_Schagerl@web.de>
2019-10-11 14:28:49 +0200

    added logging for userpoolid and client id

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,8 @@ import { AuthProvider } from './src/context/Authentication';
 // This is used to wrap the page, so we can configure AWS Cognito in this wrapper,
 // so it only gets configured once, not every time a page changes
 export const wrapRootElement = ({ element }) => {
+  console.log('userpoolid', config.COGNITO.USER_POOL_ID);
+  console.log('web client id', process.env.COGNITO_APP_CLIENT_ID);
   Amplify.configure({
     region: config.COGNITO.REGION,
     userPoolId: config.COGNITO.USER_POOL_ID,
