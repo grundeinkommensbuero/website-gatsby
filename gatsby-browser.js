7f8d9db988a42d3e96523738be2569b869d11824
Valioesi <Vali_Schagerl@web.de>
2019-10-11 14:21:13 +0200

    add modular amplify core, bring back in configure

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,17 @@
 import React from 'react';
-// import Amplify from 'aws-amplify';
-// import { config } from './aws-config';
+import Amplify from '@aws-amplify/core';
+import { config } from './aws-config';
 import { AuthProvider } from './src/context/Authentication';
 
 // This is used to wrap the page, so we can configure AWS Cognito in this wrapper,
 // so it only gets configured once, not every time a page changes
 export const wrapRootElement = ({ element }) => {
-  /*Amplify.configure({
+  Amplify.configure({
     region: config.COGNITO.REGION,
     userPoolId: config.COGNITO.USER_POOL_ID,
     userPoolWebClientId: process.env.COGNITO_APP_CLIENT_ID,
   });
-  */
+
   return <AuthProvider>{element}</AuthProvider>;
 };
 
