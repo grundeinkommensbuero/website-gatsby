b37611cb5b703124d203f898968691d365872b68
Timur Çelikel <me@timurcelikel.com>
2019-10-14 11:58:37 +0200

    text input: add wrapped component

--- src/components/Forms/TextInput/index.js
+++ src/components/Forms/TextInput/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import s from './style.module.less';
 import cN from 'classnames';
+import LabelInputErrorWrapper from '../LabelInputErrorWrapper';
 
 export function TextInput({ children, className, ...other }) {
   return <input className={cN(s.textInput, className)} {...other} />;
 }
+
+export const TextInputWrapped = ({ input, children, label, meta }) => (
+  <LabelInputErrorWrapper label={label} meta={meta}>
+    <select {...input}>{children}</select>
+  </LabelInputErrorWrapper>
+);
 
 export function TextInputInline({ children, className, ...other }) {
   return <input className={cN(s.textInputInline, className)} {...other} />;
