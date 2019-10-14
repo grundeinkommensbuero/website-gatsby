049d470cb939101fc0cfbf0b68effaaac9630ff7
Timur Çelikel <me@timurcelikel.com>
2019-10-14 11:11:00 +0200

    Button: basic button wrapper

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,8 +4,16 @@ import cN from 'classnames';
 
 export function LinkButton({ children, className, ...other }) {
   return (
-    <a className={cN(s.button, className)} {...other}>
+    <a className={cN(s.linkButton, className)} {...other}>
       {children}
     </a>
+  );
+}
+
+export function Button({ children, className, ...other }) {
+  return (
+    <button className={cN(s.button, className)} {...other}>
+      {children}
+    </button>
   );
 }
