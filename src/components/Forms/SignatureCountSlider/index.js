a3dd4d05a1d96121910fc0a8c77b2bd12bb1ceee
Timur Çelikel <me@timurcelikel.com>
2019-10-15 17:34:43 +0200

    slider: basic styling

--- src/components/Forms/SignatureCountSlider/index.js
+++ src/components/Forms/SignatureCountSlider/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import s from './style.module.less';
 
 export const SignatureCountSlider = ({ input, label, min, max }) => (
   <div>
     <label htmlFor={`slider_${input.name}`}>{label}</label>
     <br />
-    <input id={`slider_${input.name}`} min={min} max={max} {...input} />
+    <input
+      className={s.input}
+      id={`slider_${input.name}`}
+      min={min}
+      max={max}
+      {...input}
+    />
     <br />
     {input.value}
   </div>
