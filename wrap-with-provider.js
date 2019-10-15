d61ab927ce2bd3abb9db613af08dfa2cc87c7c2e
Valioesi <Vali_Schagerl@web.de>
2019-10-15 17:03:26 +0200

    chang back env variable name

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import Amplify from '@aws-amplify/auth';
-import { config } from './aws-config';
 import { AuthProvider } from './src/context/Authentication';
 
 // This is used to wrap the page, so we can configure AWS Cognito in this wrapper,
 // so it only gets configured once, not every time a page changes
-export default ({ element }) => {
-  const clientId = process.env.GATSBY_APP_CLIENT_ID;
+export default ({ element }, options) => {
+  const clientId = process.env.COGNITO_APP_CLIENT_ID;
+  /*const blub = {
+    config.COGNITO.REGION,
+  config.COGNITO.USER_POOL_ID,
+  clientId}*/
   console.log('client id', clientId);
   if (clientId) {
     console.log('configuring amplify...');
     Amplify.configure({
-      region: config.COGNITO.REGION,
-      userPoolId: config.COGNITO.USER_POOL_ID,
-      userPoolWebClientId: clientId,
+      region: options.region,
+      userPoolId: options.userPoolId,
+      userPoolWebClientId: options.appClientId,
     });
   } else {
     console.log('no userPoolWebClientId provided');
