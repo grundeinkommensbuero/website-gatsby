f673069005605bd80cf355407e115eaeed717ed5
Valioesi <Vali_Schagerl@web.de>
2019-10-15 16:02:52 +0200

    added gatsby-ssr and refactored wrapping

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,8 @@
 import wrapWithProvider from './wrap-with-provider';
 export const wrapRootElement = wrapWithProvider;
 
+/*  TODO: also needed here?
+
 window.commitHash = COMMITHASH;
 window.version = VERSION;
+*/
---
+++ wrap-with-provider.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Amplify from '@aws-amplify/auth';
+import { config } from './aws-config';
+import { AuthProvider } from './src/context/Authentication';
+
+// This is used to wrap the page, so we can configure AWS Cognito in this wrapper,
+// so it only gets configured once, not every time a page changes
+export default ({ element }) => {
+  const clientId = process.env.COGNITO_APP_CLIENT_ID;
+  console.log('client id', clientId);
+  if (clientId) {
+    console.log('configuring amplify...');
+    Amplify.configure({
+      region: config.COGNITO.REGION,
+      userPoolId: config.COGNITO.USER_POOL_ID,
+      userPoolWebClientId: clientId,
+    });
+  } else {
+    console.log('no userPoolWebClientId provided');
+  }
+
+  return <AuthProvider>{element}</AuthProvider>;
+};
