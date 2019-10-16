9ac1b062ee9825a9edbd67e545661d20d8bd8807
Timur Çelikel <me@timurcelikel.com>
2019-10-16 17:56:01 +0200

    add and style ValidationError

--- src/components/Forms/Checkbox/index.js
+++ src/components/Forms/Checkbox/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import s from './style.module.less';
+import { ValidationError } from '../ValidationError';
 
 export const Checkbox = ({ input, label, meta }) => (
   <label className={s.container}>
     <input className={s.checkbox} {...input} />
     <div className={s.label}>
       {label}
-      {meta.error && meta.touched && <div>{meta.error}</div>}
+      {meta.error && meta.touched && (
+        <ValidationError>{meta.error}</ValidationError>
+      )}
     </div>
   </label>
 );
--- src/components/Forms/TextInput/index.js
+++ src/components/Forms/TextInput/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './style.module.less';
 import cN from 'classnames';
 import LabelInputErrorWrapper from '../LabelInputErrorWrapper';
+import { ValidationError } from '../ValidationError';
 
 export function TextInput({ children, className, ...other }) {
   return <input className={cN(s.textInput, className)} {...other} />;
@@ -30,6 +31,8 @@ export const TextInputOneLine = ({ input, label, meta, placeholder }) => (
   <label style={{ display: 'block' }}>
     <span>{label}</span>
     <TextInputInline {...input} placeholder={placeholder} />
-    {meta.error && meta.touched && <div>{meta.error}</div>}
+    {meta.error && meta.touched && (
+      <ValidationError>{meta.error}</ValidationError>
+    )}
   </label>
 );
