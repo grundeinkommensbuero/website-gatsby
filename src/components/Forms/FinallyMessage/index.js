e0cbc9fa8e8c8c2b66535d77c752268ee47284a2
Timur Çelikel <me@timurcelikel.com>
2019-10-17 14:35:24 +0200

    FinallyMessage: add illustration for success

--- src/components/Forms/FinallyMessage/index.js
+++ src/components/Forms/FinallyMessage/index.js
@@ -2,17 +2,27 @@ import React, { useEffect, useRef } from 'react';
 import s from './style.module.less';
 import { scrollIntoView } from '../../utils';
 import cN from 'classnames';
+import Flowers from './flowers.svg';
+import Hat from './hat.svg';
+import Crowd1 from './crowd1.svg';
+import Crowd2 from './crowd2.svg';
 
 export const FinallyMessage = ({ state, children }) => {
   useEffect(() => {
     scrollIntoView(messageRef.current);
   });
   const messageRef = useRef(null);
-  console.log(state);
 
   return (
     <div className={s.container}>
-      {state === 'saved' && <div />}
+      {state === 'saved' && (
+        <div className={s.savedStage}>
+          <img src={Flowers} className={s.savedStageFlowers} />
+          <img src={Hat} className={s.savedStageHat} />
+          <img src={Crowd1} className={s.savedStageCrowd1} />
+          <img src={Crowd2} className={s.savedStageCrowd2} />
+        </div>
+      )}
       <div className={s.message} ref={messageRef}>
         <div
           className={cN({
