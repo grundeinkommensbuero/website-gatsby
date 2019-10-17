36180392b5dc191cffd3dbe89716235a75ecd114
Timur Çelikel <me@timurcelikel.com>
2019-10-17 12:20:05 +0200

    FinallyMessage: make stand out more

--- src/components/Forms/FinallyMessage/index.js
+++ src/components/Forms/FinallyMessage/index.js
@@ -1,17 +1,27 @@
 import React, { useEffect, useRef } from 'react';
 import s from './style.module.less';
 import { scrollIntoView } from '../../utils';
+import cN from 'classnames';
 
 export const FinallyMessage = ({ state, children }) => {
   useEffect(() => {
     scrollIntoView(messageRef.current);
   });
   const messageRef = useRef(null);
+  console.log(state);
 
   return (
     <div className={s.container}>
       {state === 'saved' && <div />}
-      <div ref={messageRef}>{children}</div>
+      <div className={s.message} ref={messageRef}>
+        <div
+          className={cN({
+            [s.blinkShort]: state === 'saved' || state === 'error',
+          })}
+        >
+          {children}
+        </div>
+      </div>
     </div>
   );
 };
