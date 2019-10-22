29c0926ff47fceb3d88abab2ca68544d0d8d2903
Timur Celikel <me@timurcelikel.com>
2019-10-22 21:36:59 +0200

    Pledge: spread hands around

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -62,3 +62,12 @@ export function scrollIntoView(element) {
   const middle = absoluteElementTop - window.innerHeight / 2;
   window.scrollTo(0, middle);
 }
+
+// https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+export function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
