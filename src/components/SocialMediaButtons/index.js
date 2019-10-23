2d2892223a0b91c5ed11f4222b3ecf3607de04d0
Timur Celikel <me@timurcelikel.com>
2019-10-23 16:52:25 +0200

    refactor out HurrayCrowd, fix verfication page bugs

--- src/components/SocialMediaButtons/index.js
+++ src/components/SocialMediaButtons/index.js
@@ -6,15 +6,16 @@ import {
   faInstagram,
   faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
+import cN from 'classnames';
 
-export default ({ children }) => {
+export default ({ children, className }) => {
   const iconSize = 'lg';
   const facebookIcon = <FontAwesomeIcon icon={faFacebook} size={iconSize} />;
   const instagramIcon = <FontAwesomeIcon icon={faInstagram} size={iconSize} />;
   const twitterIcon = <FontAwesomeIcon icon={faTwitter} size={iconSize} />;
 
   return (
-    <div className={s.container}>
+    <div className={cN(s.container, className)}>
       {children && <p>{children}</p>}
       <div className={s.iconContainer}>
         <a target="_blank" href="https://www.facebook.com/expeditionbge/">
