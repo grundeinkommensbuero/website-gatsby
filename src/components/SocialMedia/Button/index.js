0f493101ed7ad858feff9dfd6ad06907a6978771
Valioesi <Vali_Schagerl@web.de>
2019-10-25 12:42:32 +0200

    differently styled follow and share buttons

--- src/components/SocialMedia/Button/index.js
+++ src/components/SocialMedia/Button/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import s from '../style.module.less';
 
 export default props => {
   console.log('props', props);
   return (
-    <a target="_blank" href={props.link}>
+    <a
+      target="_blank"
+      href={props.link}
+      aria-label={props.label}
+      className={s.link}
+    >
       <FontAwesomeIcon icon={props.icon} size={props.iconSize} />
     </a>
   );
--- src/components/SocialMedia/Follow/index.js
+++ src/components/SocialMedia/Follow/index.js
@@ -14,21 +14,24 @@ export default ({ children, className }) => {
   return (
     <div className={cN(s.container, className)}>
       {children && <p>{children}</p>}
-      <div className={s.iconContainer}>
+      <div className={`${s.iconContainer} ${s.followButtons}`}>
         <SocialMediaButton
           icon={faFacebook}
           link="https://www.facebook.com/expeditionbge/"
           iconSize={iconSize}
+          label="Folge auf Facebook"
         />
         <SocialMediaButton
           icon={faTwitter}
           link="https://twitter.com/expeditionbge"
           iconSize={iconSize}
+          label="Folge auf Twitter"
         />
         <SocialMediaButton
           icon={faInstagram}
           link="https://www.instagram.com/expedition.bge/"
           iconSize={iconSize}
+          label="Folge auf Instagram"
         />
       </div>
     </div>
