3c0f27dcc2b346cc8826fb6b6aa7cfbb5a80fafd
Timur Celikel <me@timurcelikel.com>
2019-10-31 10:24:57 +0000

    remove console.log

--- src/components/SocialMedia/Button/index.js
+++ src/components/SocialMedia/Button/index.js
@@ -3,7 +3,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import s from '../style.module.less';
 
 export default props => {
-  console.log('props', props);
   return (
     <a
       target="_blank"
