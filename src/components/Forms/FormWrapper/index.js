406ef54d0fc6cb31063133a323d493eab88aac5c
Timur Celikel <me@timurcelikel.com>
2019-11-07 16:49:14 +0100

    add anonymous download button

--- src/components/Forms/FormWrapper/index.js
+++ src/components/Forms/FormWrapper/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import s from './style.module.less';
 import cN from 'classnames';
 
-export default ({ children, className, id, handleSubmit }) => (
-  <form onSubmit={handleSubmit} className={cN(s.form, className)}>
+export default ({ children, className, id }) => (
+  <div className={cN(s.form, className)}>
     <div className={s.jumpToAnchor} id={id} />
     {children}
-  </form>
+  </div>
 );
