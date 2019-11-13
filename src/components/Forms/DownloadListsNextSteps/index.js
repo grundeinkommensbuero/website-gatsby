43f34410fcafc7dbcb9c5c6b4f97f69ad808ad2a
Timur Celikel <me@timurcelikel.com>
2019-11-13 16:13:13 +0100

    add DownloadListsNextSteps component

---
+++ src/components/Forms/DownloadListsNextSteps/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StepList, StepListItem } from '../../StepList';
+
+export default ({ needsVerification }) => (
+  <>
+    <StepList>
+      {needsVerification && (
+        <StepListItem icon="mail">
+          Check deine Mails, klick den Link, damit du dabei bist
+        </StepListItem>
+      )}
+      <StepListItem icon="print">
+        Drucke so viele Listen aus, wie du Sammeln möchtest.
+      </StepListItem>
+      <StepListItem icon="stack">Sammel, so viel du kannst!</StepListItem>
+      <StepListItem icon="send">Ab die Post an uns geschickt.</StepListItem>
+    </StepList>
+  </>
+);
