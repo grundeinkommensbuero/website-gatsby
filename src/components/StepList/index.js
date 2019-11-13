1ffbcee33ff69fa643a2a67520ec479db1c4f4a0
Timur Celikel <me@timurcelikel.com>
2019-11-13 16:12:50 +0100

    add StepList component

---
+++ src/components/StepList/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import cN from 'classnames';
+import s from './style.module.less';
+
+const ICONS = {
+  mail: require('./mail.svg'),
+  stack: require('./stack.svg'),
+  print: require('./print.svg'),
+  send: require('./send.svg'),
+};
+
+export function StepList({ children, className }) {
+  return <ul className={cN(s.list, className)}>{children}</ul>;
+}
+
+export function StepListItem({ icon, children, className }) {
+  const iconSrc = ICONS[icon];
+
+  return (
+    <li className={cN(className, s.item)}>
+      <div className={s.iconContainer}>
+        {iconSrc && <img aria-hidden="true" className={s.icon} src={iconSrc} />}
+      </div>
+      <div>{children}</div>
+    </li>
+  );
+}
