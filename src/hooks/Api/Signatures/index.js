30a2251311c4922a556c63e54d57e1fb04b822ec
Valioesi <Vali_Schagerl@web.de>
2019-11-14 14:59:20 +0100

    added hook functions to make api call

---
+++ src/hooks/Api/Signatures/index.js
@@ -0,0 +1,122 @@
+import { useAuthentication } from '../../Authentication';
+import { useState } from 'react';
+import { config } from '../../../../aws-config';
+
+export const useSignaturesApi = () => {
+  // we are calling useState to 1) return the state and 2) pass the setState function
+  // to our createSignatureList function, so we can set the state from there
+  const [state, setState] = useState(null);
+  return [
+    state,
+    userId => createSignatureListOldUser(userId, setState),
+    formData => createSignatureListNewUser(formData, setState),
+    formData => createSignatureListAnonymous(formData, setState),
+  ];
+};
+
+//Function to create (or get) a signature list for anonymous user
+//formData does not have to hold email or userId
+const createSignatureListAnonymous = async (formData, setState) => {
+  try {
+    setState('saving');
+    const data = {};
+    //handle campaign code
+    data.campaignCode = formData.campaignCode;
+    //call function to make api request, returns signature list if successful (null otherwise)
+    const signatureList = await makeApiCall(data, setState);
+    openPdf(signatureList);
+    return signatureList;
+  } catch (error) {
+    console.log('Error while creating anonymous signature list', error);
+    setState('error');
+    return null;
+  }
+};
+
+//Function to create (or get) a signature list for (possibly) new user
+//formData needs to contain email, user is first registered through cognito
+const createSignatureListNewUser = async (formData, setState) => {
+  try {
+    setState('saving');
+
+    const [signUp] = useAuthentication();
+    // check url params, if current user came from referral (e.g newsletter)
+    const urlParams = new URLSearchParams(window.location.search);
+    // the pk_source param was generated in matomo
+    const referral = urlParams.get('pk_source');
+    //TODO: handle referral and newsletter consent
+
+    const data = {};
+    //register user
+    const userId = await signUp(formData.email);
+    if (userId !== 'userExists' && userId !== 'error') {
+      data.userId = userId;
+    } else if (userId === 'userExists') {
+      //instead of the user id we pass the email to the api
+      data.email = formData.email;
+      //setState('userExists');
+    } else {
+      setState('error');
+      return null;
+    }
+    //handle campaign code
+    data.campaignCode = formData.campaignCode;
+    //call function to make api request, returns signature list if successful (null otherwise)
+    const signatureList = await makeApiCall(data, setState);
+    openPdf(signatureList);
+    return signatureList;
+  } catch (error) {
+    console.log('Error while creating signature list', error);
+    setState('error');
+    return null;
+  }
+};
+
+//Function to create (or get) a signature list an already registered user
+//userId is passed, user is not registeres through cognito
+const createSignatureListOldUser = async (userId, setState) => {
+  try {
+    setState('saving');
+    const data = { userId: userId };
+    //handle campaign code
+    data.campaignCode = formData.campaignCode;
+    //call function to make api request, returns signature list if successful (null otherwise)
+    const signatureList = await makeApiCall(data, setState);
+    openPdf(signatureList);
+    return signatureList;
+  } catch (error) {
+    console.log('Error while creating signature list', error);
+    setState('error');
+    return null;
+  }
+};
+
+//Function which calls our api to create a (new) signature list
+//returns the list {id, url} or null
+const makeApiCall = async (data, setState) => {
+  //make api call to create new singature list and get pdf
+  const request = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+  const response = await fetch(config.API.INVOKE_URL + '/signatures', request);
+  const json = await response.json();
+  //status might also be 200 in case there already was an existing pdf
+  if (response.status === 201 || response.status === 200) {
+    setState('created');
+    return json.signatureList;
+  }
+  console.log('error', json.message);
+  setState('error');
+  return null;
+};
+
+const openPdf = signatureList => {
+  if (signatureList !== null) {
+    window.open(signatureList.url, '_blank');
+  }
+};
--- src/pages/playingAround.js
+++ src/pages/playingAround.js
@@ -1,5 +1,6 @@
 import React, { Component, useState } from 'react';
 import { useGlobalState, useAuthentication } from '../hooks/Authentication';
+import { useSignaturesApi } from '../hooks/Api/Signatures';
 
 export default class PlayingAround extends Component {
   render() {
@@ -8,23 +9,16 @@ export default class PlayingAround extends Component {
 }
 
 const TestComponent = () => {
-  const [signUp] = useAuthentication();
-  const [state, setState] = useState({ value: '' });
-  const context = useGlobalState();
-  console.log('context inside header', context);
+  const [
+    state,
+    createUserSignatureList,
+    createAnonymousSignatureList,
+  ] = useSignaturesApi();
+  console.log('state', state);
   return (
     <div>
-      <button onClick={() => signUp('valentin@grundeinkommensbuero.de')}>
-        Click me
-      </button>
-      <input
-        type="text"
-        value={state.value}
-        onChange={e => setState({ value: e.target.value })}
-      ></input>
-      <button onClick={() => answerCustomChallenge(state.value, context)}>
-        Send code
-      </button>
+      <button onClick={() => createAnonymousSignatureList({})}>Click me</button>
+      <p>{state === null ? 'no state' : state}</p>
     </div>
   );
 };
