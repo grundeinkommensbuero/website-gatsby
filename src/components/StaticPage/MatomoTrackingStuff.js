c5841ed49d6af9bd4cc6d1cf5b7e0d08fc5ed517
Timur Celikel <me@timurcelikel.com>
2019-11-19 19:57:09 +0100

    AB-test: add redirect to typeform

--- src/components/StaticPage/MatomoTrackingStuff.js
+++ src/components/StaticPage/MatomoTrackingStuff.js
@@ -1,10 +1,11 @@
 const MatomoTrackingStuff = `
-    var _paq = _paq || [];
+var _paq = _paq || [];
     _paq.push(['AbTesting::create', {
         name: '2', // you can also use '2' (ID of the experiment) to hide the name
         percentage: 100,
         includedTargets: [{"attribute":"url","inverted":"0","type":"equals_simple","value":"https:\/\/expedition-grundeinkommen.de\/brandenburg\/"}],
         excludedTargets: [],
+        startDateTime: '2019/11/18 12:51:51 UTC',
         variations: [
             {
                 name: 'original',
@@ -36,6 +37,12 @@ const MatomoTrackingStuff = `
                 name: '6', // you can also use '6' (ID of the variation) to hide the name
                 activate: function(event) {
                     event.redirect('https://expedition-grundeinkommen.de/brandenburg-5/');
+                }
+            },                        {
+                name: '7', // you can also use '7' (ID of the variation) to hide the name
+                percentage: 6,
+                activate: function(event) {
+                    event.redirect('https://expeditionbge.typeform.com/to/H9A252');
                 }
             }            
         ],
