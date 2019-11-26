0a51fce348e0edb4fb88a8217e8c94ce9cfd32f6
Timur Çelikel <me@timurcelikel.com>
2019-11-26 11:47:03 +0100

    add ab test brandenburg live

--- src/components/StaticPage/MatomoTrackingStuff.js
+++ src/components/StaticPage/MatomoTrackingStuff.js
@@ -1,3 +1,33 @@
-const MatomoTrackingStuff = ``;
+const MatomoTrackingStuff = `
+var _paq = _paq || [];
+_paq.push(['AbTesting::create', {
+    name: '3', // you can also use '3' (ID of the experiment) to hide the name
+    percentage: 100,
+    includedTargets: [{"attribute":"url","inverted":"0","type":"equals_simple","value":"https:\/\/expedition-grundeinkommen.de\/brandenburg\/"}],
+    excludedTargets: [],
+    variations: [
+        {
+            name: 'original',
+            activate: function (event) {
+                // usually nothing needs to be done here
+            }
+        },
+        {
+            name: '8', // you can also use '8' (ID of the variation) to hide the name
+            activate: function(event) {
+                event.redirect('https://expedition-grundeinkommen.de/brandenburg-1/');
+            }
+        },                        {
+            name: '9', // you can also use '9' (ID of the variation) to hide the name
+            activate: function(event) {
+                event.redirect('https://expedition-grundeinkommen.de/brandenburg-2/');
+            }
+        }
+    ],
+    trigger: function () {
+        return true; // here you can further customize which of your visitors will participate in this experiment
+    }
+}]);
+`;
 
 export default MatomoTrackingStuff;
