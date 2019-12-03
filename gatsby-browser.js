a8ab2dd5d50c3537c0eae8f9f0646eb56bbfb4b1
Timur Çelikel <me@timurcelikel.com>
2019-12-03 14:49:45 +0100

    test send beacon

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,12 @@
 import wrapWithProvider from './wrap-with-provider';
 export const wrapRootElement = wrapWithProvider;
 
+console.log('in da browser');
+
+if (window._paq) {
+  console.log('there is some paq');
+  _paq.push(['alwaysUseSendBeacon']);
+}
+
 window.commitHash = COMMITHASH;
 window.version = VERSION;
