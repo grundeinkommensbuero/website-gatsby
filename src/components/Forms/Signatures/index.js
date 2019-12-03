593d54fd72f9478fe8529191c95e0a06ee71f91c
Timur Çelikel <me@timurcelikel.com>
2019-12-03 15:00:51 +0100

    Signatures: add missing import

--- src/components/Forms/Signatures/index.js
+++ src/components/Forms/Signatures/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Form, Field } from 'react-final-form';
 import { TextInputWrapped } from '../TextInput';
-import { validateEmail, addActionTrackingId } from '../../utils';
+import { validateEmail, addActionTrackingId, trackEvent } from '../../utils';
 import s from './style.module.less';
 import { CTAButton, CTAButtonContainer } from '../../Layout/CTAButton';
 import { useCreateSignatureList } from '../../../hooks/Api/Signatures';
