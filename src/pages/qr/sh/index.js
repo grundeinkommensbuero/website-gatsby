d865f1606f71464eac4288c646dfc541e81c9149
Timur Çelikel <me@timurcelikel.com>
2019-12-10 13:03:27 +0100

    SelfScan: first UI prototype

---
+++ src/pages/qr/sh/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Layout from '../../../components/Layout';
+import {
+  SectionWrapper,
+  Section,
+  SectionInner,
+} from '../../../components/Layout/Sections';
+import SelfScan from '../../../components/Forms/SelfScan';
+import Helmet from 'react-helmet';
+
+export default () => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>Selbsteingabe Unterschriftenlist</title>
+      </Helmet>
+
+      <SectionWrapper>
+        <Section title="Trag deine Unterschriften ein">
+          <SectionInner hugeText={true}>
+            <SelfScan />
+          </SectionInner>
+        </Section>
+      </SectionWrapper>
+    </Layout>
+  );
+};
