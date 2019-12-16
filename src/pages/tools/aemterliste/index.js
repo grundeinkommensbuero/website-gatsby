8d48c2b3d731f5bed5b0613112335c4137b9f40f
Timur Çelikel <me@timurcelikel.com>
2019-12-16 17:04:02 +0100

    move Aemterliste to separate component to fix SSR

--- src/pages/tools/aemterliste/index.js
+++ src/pages/tools/aemterliste/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Layout from '../../../components/Layout';
 import Helmet from 'react-helmet';
 import {
@@ -6,41 +6,9 @@ import {
   Section,
   SectionInner,
 } from '../../../components/Layout/Sections';
-import { AEMTER, GEMEINDEN } from './aemter';
+import Aemterliste from '../../../components/Aemterliste';
 
 export default () => {
-  const [selectedAmt, setSelectedAmt] = useState(null);
-  const sorted = {};
-
-  AEMTER.forEach(amt => {
-    if (amt[1].startsWith('0')) {
-      let index = 0;
-      let amtExtended = generateAmtId(amt[1], index);
-      while (sorted[amtExtended] !== undefined) {
-        index++;
-        amtExtended = generateAmtId(amt[1], index);
-      }
-
-      sorted[amtExtended] = { amt: amt[0], gemeinden: [amt[0]] };
-    } else {
-      sorted[amt[1]] = { amt: amt[0], gemeinden: [] };
-    }
-  });
-
-  GEMEINDEN.forEach(gemeinde => {
-    if (sorted[gemeinde[1]]) {
-      sorted[gemeinde[1]].gemeinden.push(gemeinde[0]);
-    }
-  });
-
-  const gemeindenByName = {};
-
-  Object.entries(sorted).forEach(([key, amt]) => {
-    amt.gemeinden.forEach(gemeinde => {
-      gemeindenByName[gemeinde] = key;
-    });
-  });
-
   return (
     <Layout>
       <Helmet>
@@ -49,38 +17,10 @@ export default () => {
       <SectionWrapper>
         <Section title="Ämter in SH">
           <SectionInner>
-            <input
-              list="suggestionList"
-              id="answerInput"
-              onChange={e => {
-                if (gemeindenByName[e.target.value]) {
-                  setSelectedAmt(sorted[gemeindenByName[e.target.value]]);
-                }
-              }}
-            />
-            <datalist id="suggestionList">
-              {Object.keys(gemeindenByName).map(gemeinde => (
-                <option key={gemeinde} value={gemeinde} />
-              ))}
-            </datalist>
-
-            {selectedAmt && (
-              <dl>
-                <dt>Amt</dt>
-                <dd>{selectedAmt.amt}</dd>
-                <dt>Gemeinden</dt>
-                {selectedAmt.gemeinden.map(gemeinde => (
-                  <dd>{gemeinde}</dd>
-                ))}
-              </dl>
-            )}
+            <Aemterliste />
           </SectionInner>
         </Section>
       </SectionWrapper>
     </Layout>
   );
 };
-
-function generateAmtId(amtExtended, index) {
-  return `${amtExtended}-${index}`;
-}
