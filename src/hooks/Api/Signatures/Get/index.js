6d8c64ac4273cb9c6b849e782add5d3644f4db0b
Valioesi <Vali_Schagerl@web.de>
2019-12-19 14:28:15 +0100

    hook for visualisations

---
+++ src/hooks/Api/Signatures/Get/index.js
@@ -0,0 +1,50 @@
+/**
+ *  This file holds hooks (or just one) to make api calls regarding
+ *  getting data concerning signatures (e.g. count of signatures for
+ *  each campaign)
+ */
+
+import { useState } from 'react';
+import { config } from '../../../../../aws-config';
+
+/*
+  States: 
+  - undefined
+  - stats
+*/
+
+export const useSignatureCount = () => {
+  const [stats, setStats] = useState(() => {
+    getSignatureCount().then(data => setStats(data));
+  });
+
+  return stats;
+};
+
+//gets stats (count of signatures) for each campaign
+const getSignatureCount = async () => {
+  console.log('getting signature counts...');
+  try {
+    const request = {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const response = await fetch(
+      `${config.API.INVOKE_URL}/analytics/signatures`,
+      request
+    );
+
+    if (response.status === 200) {
+      //get stats (object) by parsing json { campaign1: {withMixed, withoutMixed}, campaign2: {...}}
+      return await response.json();
+    } else {
+      console.log('Response is not 200', response.status);
+    }
+  } catch (error) {
+    console.log('Error while updating signature list', error);
+  }
+};
