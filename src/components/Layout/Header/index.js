a3dffc9512e0c46fb839f71fd4418730c1a0f725
Timur Çelikel <me@timurcelikel.com>
2019-12-19 11:43:14 +0100

    Menu: get in parent points

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './style.module.less';
 import Link from 'gatsby-link';
 import Logo from './logo.svg';
+import cN from 'classnames';
 
 const Header = ({ menu }) => {
   return (
@@ -20,15 +21,13 @@ const Header = ({ menu }) => {
           <nav className={s.nav}>
             <ul className={s.navList}>
               {menu.map((item, index) => (
-                <li key={index} className={s.navItem}>
-                  <Link
-                    className={s.link}
-                    activeClassName={s.linkActive}
-                    to={item.slug === '/' ? '/' : `/${item.slug}/`}
-                  >
-                    {item.shortTitle || item.title}
-                  </Link>
-                </li>
+                <>
+                  {item.__typename === 'ContentfulStaticContent' ? (
+                    <MenuItem key={index} {...item} />
+                  ) : (
+                    <MenuItemParent key={index} {...item} />
+                  )}
+                </>
               ))}
             </ul>
           </nav>
@@ -37,5 +36,32 @@ const Header = ({ menu }) => {
     </header>
   );
 };
+
+const MenuItemParent = ({ title, contentfulchildren }) => {
+  const children = contentfulchildren;
+  return (
+    <li className={cN(s.navItem, s.navItemParent)}>
+      <div className={s.menuItemParentTitle}>{title}</div>
+      <ul className={s.menuItemParentChildren}>
+        {children.map((item, index) => (
+          <MenuItem key={index} child={true} {...item} />
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+const MenuItem = ({ slug, shortTitle, title, child }) => (
+  <li className={cN(s.navItem, { [s.navItemChild]: child })}>
+    {console.log('huhu', slug)}
+    <Link
+      className={s.link}
+      activeClassName={s.linkActive}
+      to={slug === '/' ? '/' : `/${slug}/`}
+    >
+      {shortTitle || title}
+    </Link>
+  </li>
+);
 
 export default Header;
