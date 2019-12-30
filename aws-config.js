86bba0c79020aafe629f408f43239840eeacc819
Valioesi <Vali_Schagerl@web.de>
2019-12-30 19:44:43 +0100

    endpoint /pledges instead of /pledge

--- aws-config.js
+++ aws-config.js
@@ -3,7 +3,7 @@ export default {
     REGION: 'eu-central-1',
     USER_POOL_ID:
       process.env.NODE_ENV === 'development'
-        ? 'eu-central-1_i3iswzMTa'
+        ? 'eu-central-1_L8GSIeZz3'
         : 'eu-central-1_ficPCl5Rl',
   },
   API: {
