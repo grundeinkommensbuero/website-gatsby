48cdd0f2de693a0544ef074496364d3b01822ac8
Valioesi <Vali_Schagerl@web.de>
2020-01-07 09:43:16 +0100

    changed api urls and pool ids

--- aws-config.js
+++ aws-config.js
@@ -3,13 +3,13 @@ export default {
     REGION: 'eu-central-1',
     USER_POOL_ID:
       process.env.NODE_ENV === 'development'
-        ? 'eu-central-1_L8GSIeZz3'
-        : 'eu-central-1_ficPCl5Rl',
+        ? 'eu-central-1_SYtDaO0qH'
+        : 'eu-central-1_xx4VmPPdF',
   },
   API: {
     INVOKE_URL:
       process.env.NODE_ENV === 'development'
-        ? 'https://ftwey058f0.execute-api.eu-central-1.amazonaws.com/dev'
-        : 'https://9jvbvaehkd.execute-api.eu-central-1.amazonaws.com/prod',
+        ? 'https://2j0bcp5tr9.execute-api.eu-central-1.amazonaws.com/dev'
+        : 'https://ag5gu1z06h.execute-api.eu-central-1.amazonaws.com/prod',
   },
 };
