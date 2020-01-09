cb226b30c2824c71ae0fbccf4f37cfba33115680
Timur Çelikel <me@timurcelikel.com>
2020-01-09 18:21:13 +0100

    add QR code page for brandenburg

---
+++ src/pages/qr/bb/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Layout from '../../../components/Layout';
+import {
+  SectionWrapper,
+  Section,
+  SectionInner,
+} from '../../../components/Layout/Sections';
+import SelfScan from '../../../components/Forms/SelfScan';
+import Helmet from 'react-helmet';
+
+export default () => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>Selbsteingabe Unterschriftsliste</title>
+      </Helmet>
+
+      <SectionWrapper>
+        <Section title="Unterschriften zählen">
+          <SectionInner hugeText={true}>
+            Du hast Unterschriften gesammelt? Bitte sag uns, Unterschriften
+            hinzu gekommen sind:
+          </SectionInner>
+          <SectionInner hugeText={true}>
+            <SelfScan successMessage="Danke! Bitte schicke die Listen möglichst schnell an: Expedition Grundeinkommen, Karl-Marx-Straße 50, 12043 Berlin." />
+          </SectionInner>
+        </Section>
+      </SectionWrapper>
+    </Layout>
+  );
+};
