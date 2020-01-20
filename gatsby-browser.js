d7cc6601ee4b179e6460748c8ee45bd4c424b63b
Timur Çelikel <me@timurcelikel.com>
2020-01-20 13:39:46 +0100

    add trackjs

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,9 @@
 import wrapWithProvider from './wrap-with-provider';
+import { TrackJS } from 'trackjs';
 export const wrapRootElement = wrapWithProvider;
 
 window.commitHash = COMMITHASH;
 window.version = VERSION;
+
+TrackJS.install({ token: 'bdfde1e3d32848da99ec8f12f4136b59' });
+TrackJS.track('Testing TrackJS!');
