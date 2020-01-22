0204f5519f177ca6a2ab8be0fb8a15dbf30dc64b
Timur Çelikel <me@timurcelikel.com>
2020-01-22 10:08:11 +0100

    FormWrapper: remove unused stuff

--- src/components/Forms/FormWrapper/index.js
+++ src/components/Forms/FormWrapper/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import s from './style.module.less';
 import cN from 'classnames';
 
-export default ({ children, className, id }) => (
-  <div className={cN(s.form, className)}>
-    <div className={s.jumpToAnchor} id={id} />
-    {children}
-  </div>
+export default ({ children, className }) => (
+  <div className={cN(s.form, className)}>{children}</div>
 );
