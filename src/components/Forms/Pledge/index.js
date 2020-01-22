eb3343c24c29feccfe2500bdcfd4f688804199be
Timur Çelikel <me@timurcelikel.com>
2020-01-22 10:08:38 +0100

    Pledge: add "general" pledge

--- src/components/Forms/Pledge/index.js
+++ src/components/Forms/Pledge/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import s from './style.module.less';
 import SignaturePledge from './SignaturePledge';
+import GeneralPledge from './GeneralPledge';
 
 export default ({ className, pledgeId }) => {
+  const isGeneralPlede = pledgeId.startsWith('general');
   return (
     <div className={className}>
       <div className={s.jumpToAnchorWrapper}>
         <div className={s.jumpToAnchor} id="pledge" />
       </div>
-      <SignaturePledge pledgeId={pledgeId} />
+      {isGeneralPlede ? (
+        <GeneralPledge pledgeId={pledgeId} />
+      ) : (
+        <SignaturePledge pledgeId={pledgeId} />
+      )}
     </div>
   );
 };
