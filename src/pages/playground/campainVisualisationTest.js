316e9fd0014a2f195dca3b876696bd035221020b
Timur Çelikel <me@timurcelikel.com>
2020-01-30 09:25:06 +0100

    CampaignVisualisations: more playground examples

--- src/pages/playground/campainVisualisationTest.js
+++ src/pages/playground/campainVisualisationTest.js
@@ -25,6 +25,21 @@ export default () => {
                     'Wed Jan 29 2020 16:08:17 GMT+0100 (Central European Standard Time)',
 
                   title: 'Schlewsig-Holstein',
+                  currentCount: 231,
+                  minimum: 0,
+                  addToSignatureCount: 10,
+                  showCTA: false,
+                  ctaLink: '#',
+                  eyeCatcher: false,
+                  goalInbetween: 2000,
+                  goalUnbuffered: 20000,
+                },
+                {
+                  goal: 25000,
+                  startDate:
+                    'Wed Jan 29 2020 16:08:17 GMT+0100 (Central European Standard Time)',
+
+                  title: 'Schlewsig-Holstein',
                   currentCount: 3425,
                   minimum: 0,
                   addToSignatureCount: 10,
@@ -40,13 +55,27 @@ export default () => {
                     'Wed Jan 29 2020 16:08:17 GMT+0100 (Central European Standard Time)',
 
                   title: 'Schlewsig-Holstein',
-                  currentCount: 231,
+                  currentCount: 19302,
                   minimum: 0,
                   addToSignatureCount: 10,
                   showCTA: false,
                   ctaLink: '#',
                   eyeCatcher: false,
-                  goalInbetween: 2000,
+                  goalInbetween: 25000,
+                  goalUnbuffered: 22000,
+                },
+                {
+                  goal: 25000,
+                  startDate:
+                    'Sun Mar 29 2020 16:08:17 GMT+0100 (Central European Standard Time)',
+
+                  title: 'Berlin',
+                  currentCount: 3425,
+                  minimum: 0,
+                  addToSignatureCount: 10,
+                  showCTA: false,
+                  ctaLink: '#',
+                  eyeCatcher: false,
                   goalUnbuffered: 20000,
                 },
               ]}
