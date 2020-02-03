bc54704b7d96a8c58ff4990f04c196d0618da148
Valioesi <Vali_Schagerl@web.de>
2020-02-03 18:53:39 +0100

    reinstantiated passwordless sign in hooks

--- src/hooks/Authentication/index.js
+++ src/hooks/Authentication/index.js
@@ -40,6 +40,15 @@ export const useVerification = () => {
   ];
 };
 
+// hook for signing out
+// in comparison to other hooks we only return the function, no state
+export const useSignOut = () => {
+  //get global context
+  const context = useContext(AuthContext);
+
+  return () => signOut(context);
+};
+
 // Amplifys Auth class is used to sign up user
 const signUp = async (email, setState) => {
   try {
@@ -108,36 +117,39 @@ const resendEmail = async (email, setVerificationState) => {
   }
 };
 
-/*
-  Not needed anymore until we really need a login
-
 // Sign in user through AWS Cognito (passwordless)
-const signIn = async (email, context) => {
+const signIn = async (email, setState, { setUser }) => {
   try {
     // This will initiate the custom flow, which will lead to the user receiving a mail.
     // The code will timeout after 3 minutes (enforced server side by AWS Cognito).
     const user = await Auth.signIn(email);
-    console.log('called Auth.signIn()', user);
-    //we already set the user here, because we need the object in answerCustomChallenge()
-    context.setUser(user);
-    console.log('context', context);
+
+    // We already set the user here in the global context,
+    // because we need the object in answerCustomChallenge()
+    setUser(user);
+    setState('success');
   } catch (error) {
-    //TODO: Error handling in UI?
+    if (error.code === 'UserNotFoundException') {
+      setState('userNotFound');
+    } else {
+      setState('error');
+    }
     console.log('Error while signing in', error);
   }
 };
 
 // Function to send login code to aws
-const answerCustomChallenge = async (answer, context) => {
+const answerCustomChallenge = async (
+  answer,
+  setState,
+  { user, setUser, setIsAuthenticated }
+) => {
   // Send the answer to the User Pool
   try {
-    //we want to get the user object, which we have saved in our global state,
-    //because we need to pass it to sendCustomChallengeAnswer
-    const { state, setUser, setIsAuthenticated } = context;
-    console.log('state', state);
+    setState('loading');
     // sendCustomChallengeAnswer() will throw an error if it’s the 3rd wrong answer
-    const user = await Auth.sendCustomChallengeAnswer(state.user, answer);
-    console.log('user after sending challenge', user);
+    const tempUser = await Auth.sendCustomChallengeAnswer(user, answer);
+
     // It we get here, the answer was sent successfully,
     // but it might have been wrong (1st or 2nd time)
     // So we should test if the user is authenticated now
@@ -145,14 +157,17 @@ const answerCustomChallenge = async (answer, context) => {
       // This will throw an error if the user is not yet authenticated:
       await Auth.currentSession();
       //User is now signed in
-      //use context hook to set user in global state
-      setUser(user);
+      setState('success');
+
+      //use context to set user in global state
       setIsAuthenticated(true);
+      setUser(tempUser);
     } catch (error) {
-      //TODO: Error handling in UI: wrong code
+      setState('wrongCode');
       console.log('Apparently the user did not enter the right code', error);
     }
   } catch (error) {
+    setState('wrongCode');
     console.log(
       'User entered wrong code three times or user was never set',
       error
@@ -161,19 +176,14 @@ const answerCustomChallenge = async (answer, context) => {
 };
 
 //Function, which uses the amplify api to sign out user
-export const signOut = async () => {
+const signOut = async ({ setUser, setIsAuthenticated }) => {
   try {
     await Auth.signOut();
-    //use context hook to set user in global state
-    const { setUser, setIsAuthenticated } = useContext(AuthContext);
-    setUser(null);
+
+    //use context to set user in global state
     setIsAuthenticated(false);
+    setUser(null);
   } catch (error) {
-    //TODO: Error handling in UI: Sign out error
     console.log('Error while signing out', error);
   }
 };
-
-
-
-*/
