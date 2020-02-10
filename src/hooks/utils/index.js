4fc829723e19a7bf3315a05343066250dbea1f65
Valioesi <Vali_Schagerl@web.de>
2020-02-10 15:06:18 +0100

    updated newsletter hook

--- src/hooks/utils/index.js
+++ src/hooks/utils/index.js
@@ -5,34 +5,77 @@ import CONFIG from '../../../aws-config';
  * multiple timmes) to use within the hooks
  */
 
-//Makes api call to update user in db, returns true if successful, false otherwise
-export const updateUser = async ({ userId, email }, referral) => {
+//Makes api call to update user in db, throws error if unsuccessful
+export const updateUser = async (
+  userId,
+  newsletterConsent,
+  referral,
+  token
+) => {
+  const url = `${CONFIG.API.INVOKE_URL}/users/${userId}`;
+
+  const response = await saveUser(
+    userId,
+    newsletterConsent,
+    referral,
+    'PATCH',
+    url,
+    token
+  );
+
+  if (response.status !== 204) {
+    throw new Error(`Api response was ${response.status}`);
+  }
+};
+
+//Makes api call to update user in db, throws error if unsuccessful
+export const createUser = async (userId, newsletterConsent, referral) => {
+  const url = `${CONFIG.API.INVOKE_URL}/users`;
+
+  const response = await saveUser(
+    userId,
+    newsletterConsent,
+    referral,
+    'POST',
+    url
+  );
+
+  if (response.status !== 201) {
+    throw new Error(`Api response was ${response.status}`);
+  }
+};
+
+// Helper function to saveUser, either updates via patch or creates via post
+const saveUser = async (
+  method,
+  url,
+  userId,
+  newsletterConsent,
+  referral,
+  token = null
+) => {
   //make api call to save newsletter consent and referral
-  //we don't send newsletter consent, because we set it as true anyway
-  const data = {};
+  const data = { newsletterConsent, userId };
+
   if (referral) {
     data.referral = referral;
   }
 
-  if (email) {
-    data.email = email;
-  }
-
   const request = {
-    method: 'PATCH',
+    method: method,
     mode: 'cors',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   };
-  const response = await fetch(
-    `${CONFIG.API.INVOKE_URL}/users/${userId}`,
-    request
-  );
-  if (response.status !== 204) {
-    throw new Error(`Api response was ${response.status}`);
+
+  // If there was an auth token passed add it to the headers
+  if (token) {
+    request.headers.Authorization = token;
   }
+
+  return fetch(url, request);
 };
 
 export const sleep = milliseconds => {
