946114d136181e9f42acd9b898bf2e674e481760
Timur Çelikel <me@timurcelikel.com>
2020-02-18 11:28:34 +0100

    VisualCounter: Support for changing end values

--- src/components/VisualCounter/index.js
+++ src/components/VisualCounter/index.js
@@ -1,17 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const INTERVALL = 50;
 
 export default ({ end = 0, start = 0, time = 1000 }) => {
   const [displayCount, setDisplayCount] = useState(start);
-  const distance = end - start;
+
+  const prevEndRef = useRef();
+  useEffect(() => {
+    prevEndRef.current = end;
+  });
+  const prevEnd = prevEndRef.current;
 
   useEffect(() => {
+    const startFrom = prevEnd || start;
+    const distance = end - startFrom;
     if (end !== displayCount) {
       const rounds = Math.round(time / INTERVALL);
       for (let round = 0; round <= rounds; round++) {
         setTimeout(() => {
-          setDisplayCount(Math.round((round / rounds) * distance) + start);
+          setDisplayCount(Math.round((round / rounds) * distance) + startFrom);
         }, (round / rounds) * time);
       }
     }
