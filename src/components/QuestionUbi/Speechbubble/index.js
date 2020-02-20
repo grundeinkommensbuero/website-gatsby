f9cf447a4d75b2d00b316e056b0f9f3ea4901e1e
Timur Çelikel <me@timurcelikel.com>
2020-02-20 16:15:46 +0100

    QuestionUbi: add speechbubble

---
+++ src/components/QuestionUbi/Speechbubble/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import s from './style.module.less';
+import cN from 'classnames';
+import SpeechbubbleTop from '!svg-inline-loader!./speechbubble_top.svg';
+import SpeechbubbleBottom from '!svg-inline-loader!./speechbubble_bottom.svg';
+
+export const Speechbubble = ({ children, className }) => (
+  <div className={cN(s.container, className)}>
+    <div className={s.body}>{children}</div>
+    <div
+      className={cN(s.svg, s.svgTop)}
+      dangerouslySetInnerHTML={{
+        __html: addPreserveAspectRatioToSvg(SpeechbubbleTop),
+      }}
+    />
+    <div
+      className={cN(s.svg, s.svgBottom)}
+      dangerouslySetInnerHTML={{
+        __html: addPreserveAspectRatioToSvg(SpeechbubbleBottom),
+      }}
+    />
+  </div>
+);
+
+function addPreserveAspectRatioToSvg(svg) {
+  return svg.replace('<svg', '<svg preserveAspectRatio="none"');
+}
