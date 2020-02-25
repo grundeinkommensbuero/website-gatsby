aac650f8497fe68c16f1f202b2bba527e14568b7
Timur Çelikel <me@timurcelikel.com>
2020-02-25 16:25:53 +0100

    QuestionUbi: list questions in bubbles

--- src/components/QuestionUbi/ListQuestions/index.js
+++ src/components/QuestionUbi/ListQuestions/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { SectionInner } from '../../Layout/Sections';
 import mockQuestions from './mockQuestions';
+import s from './style.module.less';
+import { Speechbubble } from '../Speechbubble';
+import AvatarImage from '../../AvatarImage';
 
 export default () => {
-  console.log(mockQuestions);
-  return <SectionInner wide={true}>hu</SectionInner>;
+  return (
+    <SectionInner wide={true}>
+      <div className={s.container}>
+        {mockQuestions.map((question, index) => (
+          <Question key={index} {...question} />
+        ))}
+      </div>
+    </SectionInner>
+  );
+};
+
+const Question = ({ user, question }) => {
+  let nameAndCity = user.name;
+  if (user.city) {
+    nameAndCity += ` aus ${user.city}`;
+  }
+  return (
+    <article className={s.question}>
+      <Speechbubble>
+        <div className={s.questionContent}>{question}</div>
+      </Speechbubble>
+      <div className={s.belowSpeechBubble}>
+        <AvatarImage className={s.avatar} user={user} />
+        <div className={s.name} title={nameAndCity}>
+          {nameAndCity}{' '}
+        </div>
+      </div>
+    </article>
+  );
 };
