873f96ed5eadf01c04a16d7dcd8eb95af9b12387
Valioesi <Vali_Schagerl@web.de>
2020-02-25 13:08:00 +0100

    pass userId to saveQuestion

--- src/hooks/Api/Questions/index.js
+++ src/hooks/Api/Questions/index.js
@@ -12,12 +12,13 @@ export const useSaveQuestion = () => {
   // we are calling useState to 1) return the state and 2) pass the setState function
   // to our saveQuestion function, so we can set the state from there
   const [state, setState] = useState(null);
-  return [state, data => saveQuestion(data, setState)];
+  return [state, (userId, data) => saveQuestion(userId, data, setState)];
 };
 
 // Function which calls the aws api to create a new question
 const saveQuestion = async (
-  { userId, question, zipCode, username },
+  userId,
+  { question, zipCode, username },
   setState
 ) => {
   try {
