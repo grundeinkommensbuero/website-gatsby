f13ebb79693450514add7a55e5a6649b3ce7a184
Timur Çelikel <me@timurcelikel.com>
2020-02-25 14:32:43 +0100

    QuestionUbi: List Questions bare component dummy content

--- src/components/QuestionUbi/index.js
+++ src/components/QuestionUbi/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CreateQuestion from './CreateQuestion';
+import ListQuestions from './ListQuestions';
 
 export default ({ mode }) => {
   return (
     <>
       <CreateQuestion />
+      <ListQuestions />
     </>
   );
 };
