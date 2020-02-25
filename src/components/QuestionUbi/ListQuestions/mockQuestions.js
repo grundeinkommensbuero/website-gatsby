0fc3eeaf7973d91ffdeef7b7ee79dd69232d58ee
Timur Çelikel <me@timurcelikel.com>
2020-02-25 14:32:43 +0100

    QuestionUbi: List Questions bare component dummy content

---
+++ src/components/QuestionUbi/ListQuestions/mockQuestions.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    user: {
+      name: 'Laura',
+      city: 'Münster',
+    },
+    question: 'Werden alle Faul auf ihrer Haut liegen?',
+  },
+  {
+    user: {
+      name: 'Joy',
+      city: 'Großstadt Freising fast München',
+    },
+    question: 'Sind das nicht die Farben der FDP?',
+  },
+  {
+    user: {
+      name:
+        'Lucia mit einem außergewöhnlich langen Namen, der nirgendwo hin passt',
+      city: 'irgendwo in Brandenburg',
+    },
+    question: 'Mir fällt keine Frage für Lucia ein. Dir vielleicht?',
+  },
+  {
+    user: {
+      name: 'Timur',
+      city: 'Bremen',
+    },
+    question:
+      'Ist das Bedingungslose Grundeinkommen eigentlich ein trojanisches Pferd der Neoliberalen?!!??!',
+  },
+  {
+    user: {
+      name: 'Vali',
+      city: 'Tübingen',
+    },
+    question: 'Gibt’s da n anständiges Sofa? Ich arbeite gern auf Sofas',
+  },
+  {
+    user: {
+      name: 'Nina',
+      city: 'Kiel',
+    },
+    question: 'Wer hat meine Banane gegessen?',
+  },
+];
