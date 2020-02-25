1bc3b617a44f83aac2bc527ab07c566a3eeb90fc
Timur Çelikel <me@timurcelikel.com>
2020-02-25 14:32:43 +0100

    QuestionUbi: List Questions bare component dummy content

---
+++ src/components/QuestionUbi/ListQuestions/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SectionInner } from '../../Layout/Sections';
+import mockQuestions from './mockQuestions';
+
+export default () => {
+  console.log(mockQuestions);
+  return <SectionInner wide={true}>hu</SectionInner>;
+};
