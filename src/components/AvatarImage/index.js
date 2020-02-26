e3562da5942635e5b1f32fd02fefac0b9722f573
Timur Çelikel <me@timurcelikel.com>
2020-02-26 09:14:08 +0100

    AvatarImage: user profile picture if given

--- src/components/AvatarImage/index.js
+++ src/components/AvatarImage/index.js
@@ -4,17 +4,32 @@ import cN from 'classnames';
 
 const avatarPlaceholders = [require('./avatar1.svg'), require('./avatar2.svg')];
 
-export default ({ user, className, src }) => {
+export default ({ user, className, srcOverwrite }) => {
   const [placeHolderImage] = useState(() => {
     return avatarPlaceholders[
       Math.round(Math.random() * (avatarPlaceholders.length - 1))
     ];
   });
+
+  const srcset =
+    user && user.profilePictures && generateSrcset(user.profilePictures);
+  const src =
+    user && user.profilePictures && Object.entries(user.profilePictures)[0][1];
+
   return (
     <img
       className={cN(s.image, className)}
-      src={src || placeHolderImage}
+      src={srcOverwrite || src || placeHolderImage}
+      srcset={srcOverwrite ? null : srcset}
       alt={user && user.name && `Avatarbild von ${user.name}`}
     />
   );
+};
+
+const generateSrcset = images => {
+  return Object.entries(images)
+    .map(([width, file]) => {
+      return `${file} ${width}w`;
+    })
+    .join(', ');
 };
