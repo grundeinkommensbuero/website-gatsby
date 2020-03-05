a57b1de6b4f6f58aaf120c4b51323a38f867d0da
Timur Çelikel <me@timurcelikel.com>
2020-03-05 09:59:00 +0100

    another experiment to lazy-load to map gl

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,5 +1,19 @@
+import React from 'react';
 import Loadable from '@loadable/component';
 
-const LoadableMap = Loadable(() => import('./Map'));
+// const LoadableMap = Loadable(() => import('./Map'));
+const LoadableMap = React.lazy(() => import('./Map'));
 
-export default LoadableMap;
+export default () => {
+  const isSSR = typeof window === 'undefined';
+  console.log('hu', isSSR);
+  return (
+    <>
+      {!isSSR && (
+        <React.Suspense fallback={<div />}>
+          <LoadableMap />
+        </React.Suspense>
+      )}
+    </>
+  );
+};
