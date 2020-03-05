c7f8f7a6c51f4ac661ae4ba4d851a71ae8144cb6
Timur Çelikel <me@timurcelikel.com>
2020-03-05 10:41:26 +0100

    Map: less jumpy loading state

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import s from './style.module.less';
+import { SectionInner } from '../Layout/Sections';
 
 const LoadableMap = React.lazy(() => import('./LazyMap'));
 
@@ -6,11 +8,19 @@ export default ({ state }) => {
   const isSSR = typeof window === 'undefined';
   return (
     <>
-      {!isSSR && (
-        <React.Suspense fallback={<div />}>
+      {isSSR ? (
+        <Fallback />
+      ) : (
+        <React.Suspense fallback={<Fallback />}>
           <LoadableMap state={state} />
         </React.Suspense>
       )}
     </>
   );
 };
+
+const Fallback = () => (
+  <SectionInner wide={true}>
+    <div className={s.container}>Lade...</div>
+  </SectionInner>
+);
