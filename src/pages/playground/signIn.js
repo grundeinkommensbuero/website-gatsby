afbc48f440a4afaa9adaa141ab18266f8f41b4d4
Timur Çelikel <me@timurcelikel.com>
2020-03-10 10:28:27 +0100

    playground signin: fix helmet import

--- src/pages/playground/signIn.js
+++ src/pages/playground/signIn.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Layout from '../../components/Layout';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import {
   SectionWrapper,
   Section,
