0323e803c50df30c94204cdb519dac360639c225
Valioesi <Vali_Schagerl@web.de>
2020-04-02 13:35:00 +0200

    hook to get data of current user

--- src/hooks/Api/Users/Get/index.js
+++ src/hooks/Api/Users/Get/index.js
@@ -3,8 +3,9 @@
  *  getting data about a specific user
  */
 
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import CONFIG from '../../../../../aws-config';
+import AuthContext from '../../../../context/Authentication';
 
 /*
   States:
@@ -13,7 +14,6 @@ import CONFIG from '../../../../../aws-config';
   - { state: 'notFound' }
   - { state: 'error' }
 */
-
 export const useUserData = () => {
   const [data, setData] = useState({});
 
@@ -48,6 +48,45 @@ const getUser = async userId => {
       return { state: 'success', user: json.user };
     } else if (response.status === 404) {
       return { state: 'notFound' };
+    }
+  } catch (error) {
+    return { state: 'error' };
+  }
+};
+
+export const useCurrentUserData = () => {
+  const [data, setData] = useState({});
+  const { token } = useContext(AuthContext);
+
+  return [
+    data,
+    () => {
+      setData({ state: 'loading' });
+      getCurrentUser(token).then(data => setData(data));
+    },
+  ];
+};
+
+// Gets data of authenticated user (in comparison to getUser including all sensitive data)
+// in the form of { state, user }
+const getCurrentUser = async token => {
+  try {
+    const request = {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    };
+
+    const response = await fetch(`${CONFIG.API.INVOKE_URL}/users/me`, request);
+
+    if (response.status === 200) {
+      const json = await response.json();
+      return { state: 'success', user: json.user };
+    } else if (response.status === 401) {
+      return { state: 'unauthorized' };
     }
   } catch (error) {
     return { state: 'error' };
