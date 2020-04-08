518c5baf113913d8bb22821ff365f9d9b881909b
Timur Çelikel <me@timurcelikel.com>
2020-04-08 11:06:10 +0200

    Crowdfunding: fix errors on production build

--- src/hooks/Api/Crowdfunding/index.js
+++ src/hooks/Api/Crowdfunding/index.js
@@ -74,34 +74,36 @@ export const useGetCrowdfundingDirectly = projectId => {
 // https://stackoverflow.com/questions/16839698/jquery-getscript-alternative-in-native-javascript
 const loadScript = (source, beforeEl, async = true, defer = true) => {
   return new Promise((resolve, reject) => {
-    let script = document.createElement('script');
-    const prior = beforeEl || document.getElementsByTagName('script')[0];
+    if (typeof window !== `undefined`) {
+      let script = document.createElement('script');
+      const prior = beforeEl || document.getElementsByTagName('script')[0];
 
-    script.async = async;
-    script.defer = defer;
+      script.async = async;
+      script.defer = defer;
 
-    function onloadHander(_, isAbort) {
-      if (
-        isAbort ||
-        !script.readyState ||
-        /loaded|complete/.test(script.readyState)
-      ) {
-        script.onload = null;
-        script.onreadystatechange = null;
-        script = undefined;
+      function onloadHander(_, isAbort) {
+        if (
+          isAbort ||
+          !script.readyState ||
+          /loaded|complete/.test(script.readyState)
+        ) {
+          script.onload = null;
+          script.onreadystatechange = null;
+          script = undefined;
 
-        if (isAbort) {
-          reject();
-        } else {
-          resolve();
+          if (isAbort) {
+            reject();
+          } else {
+            resolve();
+          }
         }
       }
+
+      script.onload = onloadHander;
+      script.onreadystatechange = onloadHander;
+
+      script.src = source;
+      prior.parentNode.insertBefore(script, prior);
     }
-
-    script.onload = onloadHander;
-    script.onreadystatechange = onloadHander;
-
-    script.src = source;
-    prior.parentNode.insertBefore(script, prior);
   });
 };
