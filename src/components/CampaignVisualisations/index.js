4d72bfa0105d1ce794fffb1401f76a612e07e210
River H <41007222+river-honer@users.noreply.github.com>
2020-05-20 13:12:58 +0100

    Preload crowdfunding data for overlay. (#107)
    * send project directly to CrowdfundingVisualisation
    
    * handle passing project as prop, or query with startnext api
    
    * remove unused, add comment
    
    * change back to empty array
    
    * check project exists in top level of props

--- src/components/CampaignVisualisations/index.js
+++ src/components/CampaignVisualisations/index.js
@@ -63,19 +63,34 @@ export default ({ visualisations }) => {
 };
 
 export const CrowdFundingVisualistation = ({ startnextId, goal, ...props }) => {
-  const [crowdFunding] = useGetCrowdfundingDirectly(startnextId);
+  // Check props includes a project key
+  const hasProjectProp = props.hasOwnProperty('project');
 
-  if (!crowdFunding) {
+  // If props does not include project key, query startnext
+  const [crowdFunding] = !hasProjectProp
+    ? useGetCrowdfundingDirectly(startnextId)
+    : [];
+
+  // If data is loading
+  if (
+    // Has project property but no value
+    (hasProjectProp && props.project == null) ||
+    // Startnext query in progress in this component
+    (!hasProjectProp && !crowdFunding)
+  ) {
     return <SectionInner>lade...</SectionInner>;
   }
 
-  const project = crowdFunding.project;
+  // Use the correct project object
+  const crowdfundingProject = hasProjectProp
+    ? props.project
+    : crowdFunding.project;
 
   return (
     <Visualisation
-      goal={project.funding_target}
-      count={Math.round(project.funding_status || 0)}
-      startDate={project.start_date}
+      goal={crowdfundingProject.funding_target}
+      count={Math.round(crowdfundingProject.funding_status || 0)}
+      startDate={crowdfundingProject.start_date}
       currency="€"
       currencyShort="€"
       showCTA={props.ctaLink}
