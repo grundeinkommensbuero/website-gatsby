ad85345b7de47872c7b9c69adcff3588940c5749
River H <river@someserver.de>
2020-05-26 17:16:38 +0200

    separate functions and create localStorageUserId hook

---
+++ src/hooks/Authentication/LocalStorageUser/index.js
@@ -0,0 +1,29 @@
+import { useState, useContext } from 'react';
+import AuthContext from '../../../context/Authentication';
+
+const USER_ID_KEY = 'user-id';
+
+export const useLocalStorageUser = () => {
+  // Reactive state for user id
+  const [userId, setUserIdState] = useState(() => {
+    // Initial value comes from localStorage
+    try {
+      console.log('Checking local storage');
+      const userId = window.localStorage.getItem(USER_ID_KEY);
+      console.log('localStorage user id', userId);
+      return userId ? JSON.parse(userId) : undefined;
+    } catch (err) {
+      console.warn('Error querying localStorage for user-id');
+      return undefined;
+    }
+  });
+
+  // Setter function updates state and localStorage
+  const setUserId = userId => {
+    console.log('Update user id', userId);
+    setUserIdState(userId);
+    window.localStorage.setItem(USER_ID_KEY, JSON.stringify(userId));
+  };
+
+  return [userId, setUserId];
+};
---
+++ src/hooks/Authentication/Verification/index.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+
+export const useVerification = () => {
+  const [verificationState, setVerificationState] = useState('verifying');
+  return [
+    verificationState,
+    (email, code) => confirmSignUp(email, code, setVerificationState),
+    email => resendEmail(email, setVerificationState),
+  ];
+};
+
+// Amplifys Auth Class is used to send a confirmation code to verify the mail address
+const confirmSignUp = async (email, confirmationCode, setVerificationState) => {
+  try {
+    const { default: Auth } = await import(
+      /* webpackChunkName: "Amplify" */ '@aws-amplify/auth'
+    );
+
+    //use auth class to confirm sing up
+    const response = await Auth.confirmSignUp(email, confirmationCode);
+    setVerificationState('verified');
+    return true;
+  } catch (error) {
+    console.log('error confirming email', error);
+    const errorCode = error.code;
+    if (errorCode === 'UserNotFoundException') {
+      setVerificationState('userNotFound');
+    } else if (errorCode === 'CodeMismatchException') {
+      setVerificationState('wrongCode');
+    } else if (errorCode === 'NotAuthorizedException') {
+      setVerificationState('alreadyVerified');
+    } else if (errorCode === 'ExpiredCodeException') {
+      setVerificationState('expiredCode');
+    } else {
+      setVerificationState('error');
+    }
+    return false;
+  }
+};
+
+// Amplifys Auth Class is used to resend the confirmation mail
+const resendEmail = async (email, setVerificationState) => {
+  try {
+    setVerificationState('resendingEmail');
+
+    const { default: Auth } = await import(
+      /* webpackChunkName: "Amplify" */ '@aws-amplify/auth'
+    );
+
+    await Auth.resendSignUp(email);
+    setVerificationState('resentEmail');
+    return true;
+  } catch (error) {
+    console.log('error resending confirmation mail');
+    setVerificationState('error');
+    //TODO: more specific error states
+    return false;
+  }
+};
