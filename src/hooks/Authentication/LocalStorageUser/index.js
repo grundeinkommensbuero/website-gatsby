fd1c0ba6adf0f1fad25f119ff084363d5744c1ab
River H <river@someserver.de>
2020-05-27 14:18:50 +0200

    clean up

--- src/hooks/Authentication/LocalStorageUser/index.js
+++ src/hooks/Authentication/LocalStorageUser/index.js
@@ -7,9 +7,7 @@ export const useLocalStorageUser = () => {
   const [userId, setUserIdState] = useState(() => {
     // Initial value comes from localStorage
     try {
-      // console.log('Checking local storage');
       const userId = window.localStorage.getItem(USER_ID_KEY);
-      // console.log('localStorage user id', userId);
       return userId ? JSON.parse(userId) : undefined;
     } catch (err) {
       console.warn('Error querying localStorage for user-id');
@@ -19,7 +17,6 @@ export const useLocalStorageUser = () => {
 
   // Setter function updates state and localStorage
   const setUserId = userId => {
-    console.log('Update user id', userId);
     setUserIdState(userId);
     window.localStorage.setItem(USER_ID_KEY, JSON.stringify(userId));
   };
