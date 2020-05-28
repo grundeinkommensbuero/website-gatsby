76cc1f8faf5c228f94aaaa970b729e6ae7fb109e
River H <river@someserver.de>
2020-05-28 17:10:42 +0200

    show avitar

--- src/components/Layout/Header/Menu/LoginMenuItem.js
+++ src/components/Layout/Header/Menu/LoginMenuItem.js
@@ -1,18 +1,35 @@
 import React, { useContext } from 'react';
+
 import AuthContext from '../../../../context/Authentication';
 import { useSignOut } from '../../../../hooks/Authentication';
+import AvatarImage from '../../../../components/AvatarImage';
 
+import s from '../style.module.less';
 import MenuItemParent from './MenuItemParent';
 import { MenuItemButton } from './MenuItem';
 
+const LoginParentTitle = ({ userData }) => {
+  if (!userData.username) {
+    return 'Log in';
+  }
+  return (
+    <div className={s.loginParentTitle}>
+      <AvatarImage className={s.loginParentAvatar} user={userData} />
+      {userData.username}
+    </div>
+  );
+};
+
 const LoginMenuItem = () => {
-  const { customUserData } = useContext(AuthContext);
+  const { customUserData: userData } = useContext(AuthContext);
   const signOut = useSignOut();
 
   return (
-    <MenuItemParent title={customUserData.username}>
-      <MenuItemButton onClick={signOut}>Log out</MenuItemButton>
-    </MenuItemParent>
+    <div className={s.loginParent}>
+      <MenuItemParent title={<LoginParentTitle userData={userData} />}>
+        <MenuItemButton onClick={signOut}>Log out</MenuItemButton>
+      </MenuItemParent>
+    </div>
   );
 };
 
