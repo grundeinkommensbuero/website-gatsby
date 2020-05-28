e1b6457eb99eec4a7afa4ce92300b1ae728f40ed
River H <river@someserver.de>
2020-05-28 13:33:44 +0200

    eslint recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
+  extends: 'eslint:recommended',
+  env: {
+    browser: true,
+    es6: true,
   },
-  "parser": "babel-eslint",
-  "plugins": [
-    "react",
-  ],
-  "globals": {
-    "graphql": false,
+  parser: 'babel-eslint',
+  plugins: ['react'],
+  globals: {
+    graphql: false,
   },
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true,
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
     },
-  }
-}
+  },
+};
