7f9d856b6ac0ee6bee8a022f864276df76ed33f9
Timur Çelikel <me@timurcelikel.com>
2020-06-03 17:51:55 +0200

    eslint: enable no-restricted-globals

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,7 @@ export const wrapRootElement = wrapWithProvider;
 window.commitHash = COMMITHASH; // eslint-disable-line no-undef
 window.version = VERSION; // eslint-disable-line no-undef
 
+// eslint-disable-next-line no-restricted-globals
 if (location.host.indexOf('localhost') !== 0) {
   TrackJS.install({
     token: 'bdfde1e3d32848da99ec8f12f4136b59',
