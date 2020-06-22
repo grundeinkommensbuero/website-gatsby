23b1055673abe3351ca2ab82b486b65481064a33
River H <river@someserver.de>
2020-06-22 12:54:48 +0200

    separate menu items and create login menu item

---
+++ src/components/Layout/Header/Menu/LoginMenuItem.jsx
@@ -0,0 +1,45 @@
+import React, { useContext } from 'react';
+
+import AuthContext from '../../../../context/Authentication';
+import { useSignOut } from '../../../../hooks/Authentication';
+import AvatarImage from '../../../../components/AvatarImage';
+
+import s from '../style.module.less';
+import MenuItemParent from './MenuItemParent';
+import { MenuItemButton } from './MenuItem';
+
+const LoginTitle = ({ userData, userId }) => {
+  // Loading state when user identified but waiting for user data
+  if (userId && !userData.username) {
+    return 'Lade...';
+  }
+  // If not identified, allow user login
+  if (!userId) {
+    // Show the login popup on click
+    return <MenuItemButton>Anmelden</MenuItemButton>;
+  }
+
+  return (
+    <div className={s.loginParentTitle}>
+      <AvatarImage className={s.loginParentAvatar} user={userData} />
+      {userData.username}
+    </div>
+  );
+};
+
+const LoginMenuItem = () => {
+  const { customUserData: userData, userId } = useContext(AuthContext);
+  const signOut = useSignOut();
+
+  return (
+    <div className={s.loginParent}>
+      <MenuItemParent
+        title={<LoginTitle userData={userData} userId={userId} />}
+      >
+        {userId && <MenuItemButton onClick={signOut}>Abmelden</MenuItemButton>}
+      </MenuItemParent>
+    </div>
+  );
+};
+
+export default LoginMenuItem;
---
+++ src/components/Layout/Header/Menu/MenuItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Link from 'gatsby-link';
+import cN from 'classnames';
+
+import s from '../style.module.less';
+
+const MenuItemLink = ({ slug, isChild, children }) => (
+  <li className={cN(s.navItem, { [s.navItemChild]: isChild })}>
+    <Link
+      className={s.link}
+      activeClassName={s.linkActive}
+      to={slug === '/' ? '/' : `/${slug}/`}
+    >
+      {children}
+    </Link>
+  </li>
+);
+
+const MenuItemButton = ({ onClick, children }) => (
+  <li className={cN(s.navItem)}>
+    <button onClick={onClick} className={s.link}>
+      {children}
+    </button>
+  </li>
+);
+
+export { MenuItemLink, MenuItemButton };
