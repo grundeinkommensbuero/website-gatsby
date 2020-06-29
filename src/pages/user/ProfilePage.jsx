d653f38b38005f62ec0d7e54dfcd35eada2f459b
River H <river@someserver.de>
2020-06-29 17:25:37 +0200

    progress on user pages

--- src/pages/user/ProfilePage.jsx
+++ src/pages/user/ProfilePage.jsx
@@ -1,18 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 
 import Layout from '../../components/Layout';
-import { Section } from '../../components/Layout/Sections';
+import {
+  Section,
+  SectionInner,
+  SectionWrapper,
+} from '../../components/Layout/Sections';
 
 import AuthContext from '../../context/Authentication';
+import { getUser } from '../../hooks/Api/Users/Get';
 
-const ProfilePage = () => {
-  const { userId, isAuthenticated } = useContext(AuthContext);
+// const getUserData = async slugId => {
+//   const response = await getUser(slugId);
+//   if ((response.status = 'success')) return response.user;
+//   return undefined;
+// };
+
+const ProfilePage = ({ id: slugId }) => {
+  const [displayUserData, setDisplayUserData] = useState();
+  const { userId, isAuthenticated, customUserData } = useContext(AuthContext);
+
+  // Get user data on page load
+  useEffect(() => {
+    if (userId && userId === slugId) {
+      setDisplayUserData(customUserData);
+    } else {
+      getUser(slugId).then(({ state, user }) => {
+        if (state === 'success') setDisplayUserData(user);
+      });
+    }
+  }, []);
+
+  console.log(displayUserData);
+
+  if (!displayUserData) return null;
 
   return (
     <Layout>
-      <Section>
-        <p>{userId}</p>
-      </Section>
+      <SectionWrapper>
+        <Section title={displayUserData.username}>
+          <SectionInner>
+            <p>Hewwo</p>
+          </SectionInner>
+        </Section>
+      </SectionWrapper>
     </Layout>
   );
 };
