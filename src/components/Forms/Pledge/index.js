50896f0b4a20cda80e82ea5d1746d36b5e94de50
Valioesi <Vali_Schagerl@web.de>
2020-07-09 16:43:43 +0200

    Removed condition to render GeneralPledge

--- src/components/Forms/Pledge/index.js
+++ src/components/Forms/Pledge/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import s from './style.module.less';
 import SignaturePledge from './SignaturePledge';
-import GeneralPledge from './GeneralPledge';
 
 export default ({ className, pledgeId }) => {
-  const isGeneralPlede = pledgeId.startsWith('general');
   return (
     <div className={className}>
       <div className={s.jumpToAnchorWrapper}>
         <div className={s.jumpToAnchor} id="pledge" />
       </div>
-      {isGeneralPlede ? (
-        <GeneralPledge pledgeId={pledgeId} />
-      ) : (
-        <SignaturePledge pledgeId={pledgeId} />
-      )}
+      <SignaturePledge pledgeId={pledgeId} />
     </div>
   );
 };
