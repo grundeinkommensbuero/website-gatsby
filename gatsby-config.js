690bb0047b5199b9c03d2ed478fd46c0d1b69399
Tatiana Schär <tsc@spacesquad.de>
2020-07-09 15:47:02 +0200

    Changed gatsby-config to add option to change environment

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   host: process.env.CONTENTFUL_HOST,
+  environment: process.env.CONTENTFUL_ENVIRONMENT,
   forceFullSync: true,
 };
 
