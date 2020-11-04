1168756548eef4faed41ea8e61125003863f19cb
Tatiana Schär <tsc@spacesquad.de>
2020-11-04 12:12:32 +0100

    Finished form, added API call

--- src/components/Forms/DonationForm/index.js
+++ src/components/Forms/DonationForm/index.js
@@ -1,69 +1,140 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import { Form, Field } from 'react-final-form';
 import { validateEmail } from '../../utils';
 import { TextInputWrapped } from '../TextInput';
 import FormSection from '../FormSection';
 import { CTAButtonContainer, CTAButton } from '../../Layout/CTAButton';
 import FormWrapper from '../FormWrapper';
+import AuthContext from '../../../context/Authentication';
+import { useUpdateUser } from '../../../hooks/Api/Users/Update';
+
+import s from './style.module.less';
 
 export default() => {
 
+    const [isRecurring, setIsRecurring] = useState(false);
+    const [isDonating, setIsDonating] = useState(false);
+    const { userId } = useContext(AuthContext);
+    const [updateUserState, updateUser] = useUpdateUser();
+
+    // const [donationInfo, setDonationInfo] = useState({ amount: 0, recurring: false, firstname: "", lastname: "", iban: ""})
+   
     return(
         <Form
-        onSubmit={e => {}}
+        onSubmit={data => {
+            console.log("formData", data);
+          const donationUpdate = { userId: userId, donation: {} };
+          donationUpdate.donation.amount = data.customAmount ? data.customAmount : data.amount
+          donationUpdate.donation.amount = parseInt (donationUpdate.donation.amount)
+          donationUpdate.donation.recurring = isRecurring
+          donationUpdate.donation.firstname = data.firstname
+          donationUpdate.donation.lastname = data.lastname
+          donationUpdate.donation.iban = data.iban
+        
+          
+          console.log("Data for API", donationUpdate);
+          console.log(updateUser(donationUpdate))
+          
+
+          }}
         render={({ handleSubmit }) => {
             return (
                 <FormWrapper>
                     <form onSubmit={handleSubmit}>
-                        <h3>Unterstütze uns!</h3>
-                        <p>Wir finanzieren uns komplett durch Spenden. Bitte hilf uns, die Expedition am Laufen zu halten!</p>
-                        <p>Unterstütze uns mit</p>
+                        {isDonating === false && (<div className={s.partialForm}>
+                            <h3>Unterstütze uns!</h3>
+                            <p>Wir finanzieren uns komplett durch Spenden. Bitte hilf uns, die Expedition am Laufen zu halten!</p>
+                            <p>Unterstütze uns mit</p>
 
-                        <FormSection>
-                            <label>
-                                <Field
-                                name="amount"
-                                component="input"
-                                type="radio"
-                                value="1"
-                                />{' '}
-                                1€
-                            </label><br></br>
-                            <label>
-                                <Field
-                                name="amount"
-                                component="input"
-                                type="radio"
-                                value="5"
-                                />{' '}
-                                5€
-                            </label><br></br>
-                            <label>
-                                <Field
-                                name="amount"
-                                component="input"
-                                type="radio"
-                                value="10"
-                                />{' '}
-                                10€
-                            </label><br></br>
+                            <FormSection>
+                                <label>
+                                    <Field
+                                    name="amount"
+                                    component="input"
+                                    type="radio"
+                                    value="1"
+                                    />{' '}
+                                        1€
+                                    </label><br></br>
+                                <label>
+                                    <Field
+                                    name="amount"
+                                    component="input"
+                                    type="radio"
+                                    value="5"
+                                    />{' '}
+                                                5€
+                                    </label><br></br>
+                                <label>
+                                    <Field
+                                    name="amount"
+                                    component="input"
+                                    type="radio"
+                                    value="10"
+                                    />{' '}
+                                                10€
+                                    </label><br></br>
 
-                            <Field
-                            name="customAmount"
-                            label="Eigener Betrag:"
-                            placeholder="100"
-                            type="text"
-                            component={TextInputWrapped}
-                            /> €
+                                <Field
+                                    name="customAmount"
+                                    label="Eigener Betrag:"
+                                    placeholder="100"
+                                    type="number"
+                                    component={TextInputWrapped}
+                                /> €
+                            </FormSection>
 
-                            {/* <Field
-                            name="city"
-                            label="Ort"
-                            placeholder="Stadt / Dorf"
-                            type="text"
-                            component={TextInputWrapped}
-                            /> */}
-                        </FormSection>
+                            <CTAButtonContainer className={s.buttonContainer}>
+                                <CTAButton onClick={() => {
+                                    setIsDonating(true);
+                                    setIsRecurring(true)
+                                    }} size="MEDIUM">
+                                    Monatlich unterstützen
+                                </CTAButton>
+                                <CTAButton onClick={() => {
+                                    setIsDonating(true);
+                                    setIsRecurring(false)
+                                    }} size="MEDIUM">
+                                    Einmalig spenden
+                                </CTAButton>
+                            </CTAButtonContainer>
+                        </div>)}
+
+
+                        {isDonating === true && (<div className={s.partialForm}>
+                            <h3>Bitte gib deine Zahlungsinformationen ein</h3>
+
+                            <FormSection>
+                                <Field
+                                    name="firstname"
+                                    label="Vorname"
+                                    placeholder="Vorname"
+                                    type="text"
+                                    component={TextInputWrapped}
+                                />
+                                <Field
+                                    name="lastname"
+                                    label="Nachname"
+                                    placeholder="Nachname"
+                                    type="text"
+                                    component={TextInputWrapped}
+                                />
+                                <Field
+                                    name="iban"
+                                    label="IBAN"
+                                    placeholder="IBAN"
+                                    type="text"
+                                    component={TextInputWrapped}
+                                />
+                            </FormSection>
+
+                            <CTAButtonContainer className={s.buttonContainer}>
+                                <CTAButton type="submit" size="MEDIUM">
+                                    Jetzt spenden
+                                </CTAButton>
+                            </CTAButtonContainer>
+                        </div>)}
+                        
                     </form>
                 </FormWrapper>
             )
