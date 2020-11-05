dfce8214bd8c60d9154962e0159272b5e46f1ae5
Tatiana Schär <tsc@spacesquad.de>
2020-11-05 11:34:35 +0100

    Added confirmation page and back buttons

--- src/components/Forms/DonationForm/index.js
+++ src/components/Forms/DonationForm/index.js
@@ -1,9 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Form, Field } from 'react-final-form';
-import { TextInputWrapped } from '../TextInput';
-import FormSection from '../FormSection';
-import { CTAButtonContainer, CTAButton } from '../../Layout/CTAButton';
 import FormWrapper from '../FormWrapper';
+import FormSection from '../FormSection';
+import { InlineButton } from '../Button';
+import { CTAButtonContainer, CTAButton } from '../../Layout/CTAButton';
+import { TextInputWrapped } from '../TextInput';
 import AuthContext from '../../../context/Authentication';
 import { useUpdateUser } from '../../../hooks/Api/Users/Update';
 
@@ -11,10 +12,12 @@ import s from './style.module.less';
 
 export default () => {
   const [isRecurring, setIsRecurring] = useState(false);
-  const [isDonating, setIsDonating] = useState(false);
+  const [enteredAmount, setEnteredAmount] = useState(false);
+  const [enteredPaymentInfo, setEnteredPaymentInfo] = useState(false);
   const [hasDonated, setHasDonated] = useState(false);
   const { userId } = useContext(AuthContext);
   const [updateUserState, updateUser] = useUpdateUser();
+  const [donationInfo, setDonationInfo] = useState();
 
   useEffect(() => {
     if (updateUserState === 'updated') {
@@ -22,26 +25,36 @@ export default () => {
     }
   }, [updateUserState]);
 
+  const Condition = ({ when, is, children }) => (
+    <Field name={when} subscription={{ value: true }}>
+      {({ input: { value } }) => (value === is ? children : null)}
+    </Field>
+  );
+
   return (
     <>
-      {!hasDonated ? (
+      {!hasDonated && !enteredPaymentInfo && (
         <Form
           onSubmit={data => {
+            console.log(data);
             const { customAmount, amount, ...inputData } = data;
-            const finalAmount = customAmount ? +customAmount : +amount;
+            const finalAmount =
+              amount === 'custom' && customAmount ? +customAmount : +amount;
+
             const donation = {
               ...inputData,
               amount: finalAmount,
               recurring: isRecurring,
             };
-            const donationUpdate = { userId: userId, donation };
-            updateUser(donationUpdate);
+            const donationInfo = { userId: userId, donation };
+            setDonationInfo(donationInfo);
+            setEnteredPaymentInfo(true);
           }}
           render={({ handleSubmit }) => {
             return (
               <FormWrapper>
                 <form onSubmit={handleSubmit}>
-                  {isDonating === false && (
+                  {enteredAmount === false && (
                     <div className={s.partialForm}>
                       <h3>Unterstütze uns!</h3>
                       <p>
@@ -81,20 +94,35 @@ export default () => {
                           10€
                         </label>
                         <br></br>
-                        <Field
-                          name="customAmount"
-                          label="Eigener Betrag:"
-                          placeholder="100"
-                          type="number"
-                          component={TextInputWrapped}
-                        />{' '}
-                        €<br></br>
+                        <label>
+                          <Field
+                            name="amount"
+                            component="input"
+                            type="radio"
+                            value="custom"
+                          />
+                          Eigener Betrag{' '}
+                        </label>
+
+                        <Condition when="amount" is="custom">
+                          <div>
+                            <Field
+                              name="customAmount"
+                              placeholder="100"
+                              type="number"
+                              component={TextInputWrapped}
+                            />{' '}
+                            €
+                          </div>
+                        </Condition>
+
+                        <br></br>
                       </FormSection>
 
                       <CTAButtonContainer className={s.buttonContainer}>
                         <CTAButton
                           onClick={() => {
-                            setIsDonating(true);
+                            setEnteredAmount(true);
                             setIsRecurring(true);
                           }}
                           size="MEDIUM"
@@ -103,7 +131,7 @@ export default () => {
                         </CTAButton>
                         <CTAButton
                           onClick={() => {
-                            setIsDonating(true);
+                            setEnteredAmount(true);
                             setIsRecurring(false);
                           }}
                           size="MEDIUM"
@@ -114,7 +142,7 @@ export default () => {
                     </div>
                   )}
 
-                  {isDonating === true && (
+                  {enteredAmount === true && (
                     <div className={s.partialForm}>
                       <h3>Bitte gib deine Zahlungsinformationen ein</h3>
 
@@ -144,8 +172,16 @@ export default () => {
 
                       <CTAButtonContainer className={s.buttonContainer}>
                         <CTAButton type="submit" size="MEDIUM">
-                          Jetzt spenden
+                          Weiter
                         </CTAButton>
+                        <InlineButton
+                          onClick={() => {
+                            setIsRecurring(false);
+                            setEnteredAmount(false);
+                          }}
+                        >
+                          Zurück
+                        </InlineButton>
                       </CTAButtonContainer>
                     </div>
                   )}
@@ -154,15 +190,60 @@ export default () => {
             );
           }}
         ></Form>
-      ) : (
+      )}
+
+      {!hasDonated && enteredPaymentInfo && (
+        <div className={s.partialForm}>
+          <h3>Bitte überprüfe deine Daten</h3>
+          <p>
+            Name:{' '}
+            <span>
+              {donationInfo.donation.firstname} {donationInfo.donation.lastname}
+            </span>
+          </p>
+          <p>
+            IBAN: <span>{donationInfo.donation.iban}</span>
+          </p>
+          <p>
+            Du spendest{' '}
+            <span>
+              {isRecurring ? 'monatlich' : 'einmalig'}{' '}
+              {donationInfo.donation.amount} €
+            </span>{' '}
+            an die Expedition.
+          </p>
+
+          <CTAButtonContainer className={s.buttonContainer}>
+            <CTAButton
+              onClick={() => {
+                updateUser(donationInfo);
+                setHasDonated(true);
+              }}
+              size="MEDIUM"
+            >
+              Jetzt spenden
+            </CTAButton>
+            <InlineButton
+              onClick={() => {
+                setEnteredPaymentInfo(false);
+              }}
+            >
+              Zurück
+            </InlineButton>
+          </CTAButtonContainer>
+        </div>
+      )}
+
+      {hasDonated && (
         <div>
           <h3>Danke für deine Spende!</h3>
           <p>Lorem ipsum … </p>
           <CTAButtonContainer className={s.buttonContainer}>
             <CTAButton
               onClick={() => {
-                setIsDonating(false);
                 setIsRecurring(false);
+                setEnteredAmount(false);
+                setEnteredPaymentInfo(false);
                 setHasDonated(false);
               }}
               size="MEDIUM"
