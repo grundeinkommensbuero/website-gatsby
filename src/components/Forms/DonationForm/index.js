828fd38bbd88943799b455120ab8e7a3c6851eea
felixbuchholz <felixbuchholz@gmx.net>
2020-11-05 10:00:31 +0100

    Add donation confirmation, refactor creating the update object

--- src/components/Forms/DonationForm/index.js
+++ src/components/Forms/DonationForm/index.js
@@ -1,6 +1,5 @@
-import React, {useState, useContext} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Form, Field } from 'react-final-form';
-import { validateEmail } from '../../utils';
 import { TextInputWrapped } from '../TextInput';
 import FormSection from '../FormSection';
 import { CTAButtonContainer, CTAButton } from '../../Layout/CTAButton';
@@ -10,135 +9,169 @@ import { useUpdateUser } from '../../../hooks/Api/Users/Update';
 
 import s from './style.module.less';
 
-export default() => {
+export default () => {
+  const [isRecurring, setIsRecurring] = useState(false);
+  const [isDonating, setIsDonating] = useState(false);
+  const [hasDonated, setHasDonated] = useState(false);
+  const { userId } = useContext(AuthContext);
+  const [updateUserState, updateUser] = useUpdateUser();
 
-    const [isRecurring, setIsRecurring] = useState(false);
-    const [isDonating, setIsDonating] = useState(false);
-    const { userId } = useContext(AuthContext);
-    const [updateUserState, updateUser] = useUpdateUser();
+  useEffect(() => {
+    if (updateUserState === 'updated') {
+      setHasDonated(true);
+    }
+  }, [updateUserState]);
 
-    // const [donationInfo, setDonationInfo] = useState({ amount: 0, recurring: false, firstname: "", lastname: "", iban: ""})
-   
-    return(
+  return (
+    <>
+      {!hasDonated ? (
         <Form
-        onSubmit={data => {
-            console.log("formData", data);
-          const donationUpdate = { userId: userId, donation: {} };
-          donationUpdate.donation.amount = data.customAmount ? data.customAmount : data.amount
-          donationUpdate.donation.amount = parseInt (donationUpdate.donation.amount)
-          donationUpdate.donation.recurring = isRecurring
-          donationUpdate.donation.firstname = data.firstname
-          donationUpdate.donation.lastname = data.lastname
-          donationUpdate.donation.iban = data.iban
-        
-          
-          console.log("Data for API", donationUpdate);
-          console.log(updateUser(donationUpdate))
-          
-
+          onSubmit={data => {
+            const { customAmount, amount, ...inputData } = data;
+            const finalAmount = customAmount ? +customAmount : +amount;
+            const donation = {
+              ...inputData,
+              amount: finalAmount,
+              recurring: isRecurring,
+            };
+            const donationUpdate = { userId: userId, donation };
+            updateUser(donationUpdate);
           }}
-        render={({ handleSubmit }) => {
+          render={({ handleSubmit }) => {
             return (
-                <FormWrapper>
-                    <form onSubmit={handleSubmit}>
-                        {isDonating === false && (<div className={s.partialForm}>
-                            <h3>Unterstütze uns!</h3>
-                            <p>Wir finanzieren uns komplett durch Spenden. Bitte hilf uns, die Expedition am Laufen zu halten!</p>
-                            <p>Unterstütze uns mit</p>
+              <FormWrapper>
+                <form onSubmit={handleSubmit}>
+                  {isDonating === false && (
+                    <div className={s.partialForm}>
+                      <h3>Unterstütze uns!</h3>
+                      <p>
+                        Wir finanzieren uns komplett durch Spenden. Bitte hilf
+                        uns, die Expedition am Laufen zu halten!
+                      </p>
+                      <p>Unterstütze uns mit</p>
 
-                            <FormSection>
-                                <label>
-                                    <Field
-                                    name="amount"
-                                    component="input"
-                                    type="radio"
-                                    value="1"
-                                    />{' '}
-                                        1€
-                                    </label><br></br>
-                                <label>
-                                    <Field
-                                    name="amount"
-                                    component="input"
-                                    type="radio"
-                                    value="5"
-                                    />{' '}
-                                                5€
-                                    </label><br></br>
-                                <label>
-                                    <Field
-                                    name="amount"
-                                    component="input"
-                                    type="radio"
-                                    value="10"
-                                    />{' '}
-                                                10€
-                                    </label><br></br>
+                      <FormSection>
+                        <label>
+                          <Field
+                            name="amount"
+                            component="input"
+                            type="radio"
+                            value="1"
+                          />{' '}
+                          1€
+                        </label>
+                        <br></br>
+                        <label>
+                          <Field
+                            name="amount"
+                            component="input"
+                            type="radio"
+                            value="5"
+                          />{' '}
+                          5€
+                        </label>
+                        <br></br>
+                        <label>
+                          <Field
+                            name="amount"
+                            component="input"
+                            type="radio"
+                            value="10"
+                          />{' '}
+                          10€
+                        </label>
+                        <br></br>
+                        <Field
+                          name="customAmount"
+                          label="Eigener Betrag:"
+                          placeholder="100"
+                          type="number"
+                          component={TextInputWrapped}
+                        />{' '}
+                        €<br></br>
+                      </FormSection>
 
-                                <Field
-                                    name="customAmount"
-                                    label="Eigener Betrag:"
-                                    placeholder="100"
-                                    type="number"
-                                    component={TextInputWrapped}
-                                /> €
-                            </FormSection>
+                      <CTAButtonContainer className={s.buttonContainer}>
+                        <CTAButton
+                          onClick={() => {
+                            setIsDonating(true);
+                            setIsRecurring(true);
+                          }}
+                          size="MEDIUM"
+                        >
+                          Monatlich unterstützen
+                        </CTAButton>
+                        <CTAButton
+                          onClick={() => {
+                            setIsDonating(true);
+                            setIsRecurring(false);
+                          }}
+                          size="MEDIUM"
+                        >
+                          Einmalig spenden
+                        </CTAButton>
+                      </CTAButtonContainer>
+                    </div>
+                  )}
 
-                            <CTAButtonContainer className={s.buttonContainer}>
-                                <CTAButton onClick={() => {
-                                    setIsDonating(true);
-                                    setIsRecurring(true)
-                                    }} size="MEDIUM">
-                                    Monatlich unterstützen
-                                </CTAButton>
-                                <CTAButton onClick={() => {
-                                    setIsDonating(true);
-                                    setIsRecurring(false)
-                                    }} size="MEDIUM">
-                                    Einmalig spenden
-                                </CTAButton>
-                            </CTAButtonContainer>
-                        </div>)}
+                  {isDonating === true && (
+                    <div className={s.partialForm}>
+                      <h3>Bitte gib deine Zahlungsinformationen ein</h3>
 
+                      <FormSection>
+                        <Field
+                          name="firstname"
+                          label="Vorname"
+                          placeholder="Vorname"
+                          type="text"
+                          component={TextInputWrapped}
+                        />
+                        <Field
+                          name="lastname"
+                          label="Nachname"
+                          placeholder="Nachname"
+                          type="text"
+                          component={TextInputWrapped}
+                        />
+                        <Field
+                          name="iban"
+                          label="IBAN"
+                          placeholder="IBAN"
+                          type="text"
+                          component={TextInputWrapped}
+                        />
+                      </FormSection>
 
-                        {isDonating === true && (<div className={s.partialForm}>
-                            <h3>Bitte gib deine Zahlungsinformationen ein</h3>
-
-                            <FormSection>
-                                <Field
-                                    name="firstname"
-                                    label="Vorname"
-                                    placeholder="Vorname"
-                                    type="text"
-                                    component={TextInputWrapped}
-                                />
-                                <Field
-                                    name="lastname"
-                                    label="Nachname"
-                                    placeholder="Nachname"
-                                    type="text"
-                                    component={TextInputWrapped}
-                                />
-                                <Field
-                                    name="iban"
-                                    label="IBAN"
-                                    placeholder="IBAN"
-                                    type="text"
-                                    component={TextInputWrapped}
-                                />
-                            </FormSection>
-
-                            <CTAButtonContainer className={s.buttonContainer}>
-                                <CTAButton type="submit" size="MEDIUM">
-                                    Jetzt spenden
-                                </CTAButton>
-                            </CTAButtonContainer>
-                        </div>)}
-                        
-                    </form>
-                </FormWrapper>
-            )
-        }} 
+                      <CTAButtonContainer className={s.buttonContainer}>
+                        <CTAButton type="submit" size="MEDIUM">
+                          Jetzt spenden
+                        </CTAButton>
+                      </CTAButtonContainer>
+                    </div>
+                  )}
+                </form>
+              </FormWrapper>
+            );
+          }}
         ></Form>
-    );
+      ) : (
+        <div>
+          <h3>Danke für deine Spende!</h3>
+          <p>Lorem ipsum … </p>
+          <CTAButtonContainer className={s.buttonContainer}>
+            <CTAButton
+              onClick={() => {
+                setIsDonating(false);
+                setIsRecurring(false);
+                setHasDonated(false);
+              }}
+              size="MEDIUM"
+            >
+              Zurück zum Formular
+            </CTAButton>
+          </CTAButtonContainer>
+        </div>
+      )}
+    </>
+  );
 };
