604ebc60d9262e300501b6f86453092113a04c6b
Tatiana Schär <tsc@spacesquad.de>
2020-11-24 11:34:22 +0100

    Added bmbf logo

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -3,22 +3,42 @@ import Link from 'gatsby-link';
 import s from './style.module.less';
 import SocialMediaButtons from '../../SocialMedia/Follow';
 
+const bmbfLogo = require('./bmbf-logo.jpg');
+
 export default ({ footerText, footerMenu }) => (
-  <footer className={s.footer}>
-    <div className={s.columnLeft}>
-      <div className={s.footerText}>{footerText}</div>
-      <SocialMediaButtons className={s.socialMedia} />
+
+  <footer>
+    <div className={s.footer}>
+      <div className={s.columnLeft}>
+        <div className={s.footerText}>{footerText}</div>
+        <SocialMediaButtons className={s.socialMedia} />
+        <img
+          aria-hidden="true"
+          alt=""
+          src={bmbfLogo}
+          className={s.bmbfLogo}
+        />
+      </div>
+      <nav className={s.columnRight}>
+        <ul className={s.nav}>
+          {footerMenu.map((item, index) => (
+            <li className={s.navItem} key={index}>
+              <Link to={`/${item.slug}/`} className={s.link}>
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
-    <nav className={s.columnRight}>
-      <ul className={s.nav}>
-        {footerMenu.map((item, index) => (
-          <li className={s.navItem} key={index}>
-            <Link to={`/${item.slug}/`} className={s.link}>
-              {item.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
+
+    <div className={s.preFooter}>
+      <img
+          aria-hidden="true"
+          alt=""
+          src={bmbfLogo}
+          className={s.bmbfLogo}
+        />
+    </div>
   </footer>
 );
