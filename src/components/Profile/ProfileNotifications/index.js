faae79327af1331903545bafa119faf07b8216bf
felixbuchholz <felixbuchholz@gmx.net>
2020-11-30 14:43:29 +0100

    add handlePlaceSelect

--- src/components/Profile/ProfileNotifications/index.js
+++ src/components/Profile/ProfileNotifications/index.js
@@ -15,50 +15,61 @@ const userCustomNewslettersFromDB = [
     value: true,
     extraInfo: false,
     timestamp: '2020-11-20T12:35:42.218Z',
-    key: 'abc-67890'
+    key: 'abc-67890',
   },
   {
     city: 'Eberswalde',
     value: true,
     extraInfo: true,
     timestamp: '2020-11-21T12:35:42.218Z',
-    key: 'abc-12345'
-  }
-]
+    key: 'abc-12345',
+  },
+];
 
 const newNewsletter = {
   city: 'Hamburg',
   value: true,
   extraInfo: true,
   timestamp: '2020-11-23T12:35:42.218Z',
-  key: 'abc-24680'
-}
+  key: 'abc-24680',
+};
 
 export default ({ userData, userId }) => {
   const [mainNewsletterConsent, updateMainNewsletterConsent] = useState({});
-  const [customNewsletterSettings, updateCustomNewsletterSettings] = useState(userCustomNewslettersFromDB);
-
+  const [customNewsletterSettings, updateCustomNewsletterSettings] = useState(
+    userCustomNewslettersFromDB
+  );
+  const [muncipality, setMuncipality] = useState();
   // userData is undefined on intitial pageload, fill state when it is ready:
-  if (userData && userData.newsletterConsent && (mainNewsletterConsent.value === undefined)) {
+  if (
+    userData &&
+    userData.newsletterConsent &&
+    mainNewsletterConsent.value === undefined
+  ) {
     updateMainNewsletterConsent(userData.newsletterConsent);
   }
 
   const revokeMainNewsletterConsent = () => {
     let updatedMainNewsletterConsent = {
-      ...mainNewsletterConsent
-    }
+      ...mainNewsletterConsent,
+    };
     updatedMainNewsletterConsent.value = !mainNewsletterConsent.value;
     updateMainNewsletterConsent(updatedMainNewsletterConsent);
-  }
+  };
 
-  const addNewsletter = (newsletterToAdd) => {
+  const handlePlaceSelect = muncipality => {
+    console.log(muncipality);
+    setMuncipality(muncipality);
+  };
+
+  const addNewsletter = newsletterToAdd => {
     let updatedNewsletter = [...customNewsletterSettings];
     updatedNewsletter.push(newsletterToAdd);
     updateCustomNewsletterSettings(updatedNewsletter);
-  }
+  };
 
-  const activeNewsletterCards = customNewsletterSettings.map((newsletter) => {
-    return <NewsletterCard newsletter={newsletter} key={newsletter.key} />
+  const activeNewsletterCards = customNewsletterSettings.map(newsletter => {
+    return <NewsletterCard newsletter={newsletter} key={newsletter.key} />;
   });
 
   return (
@@ -70,45 +81,64 @@ export default ({ userData, userId }) => {
         </div>
 
         <h3 className={s.sectionHeadline}>Newsletter & Kontakt</h3>
-        <h4 className={gS.optionSectionHeading}>Deine abonnierten Newsletter</h4>
-        {userData && userData.newsletterConsent ?
+        <h4 className={gS.optionSectionHeading}>
+          Deine abonnierten Newsletter
+        </h4>
+        {userData && userData.newsletterConsent ? (
           <section>
             {/* Main Card always visible */}
             <div className={s.newsletterCard}>
-              <p className={s.newsletterCardHeading}>Allgemeiner Expeditions-Letter</p>
-              <p className={s.newsletterCardDescription}>Du erhälst die wichtigsten Infos über die Expedition.</p>
+              <p className={s.newsletterCardHeading}>
+                Allgemeiner Expeditions-Letter
+              </p>
+              <p className={s.newsletterCardDescription}>
+                Du erhälst die wichtigsten Infos über die Expedition.
+              </p>
               <p className={cN(gS.alignRight, gS.noMargin)}>
-                {mainNewsletterConsent && mainNewsletterConsent.value ?
+                {mainNewsletterConsent && mainNewsletterConsent.value ? (
                   <span
                     aria-hidden="true"
                     className={gS.linkLikeFormated}
                     onClick={revokeMainNewsletterConsent}
-                    onKeyDown={revokeMainNewsletterConsent}>abbestellen</span>
-                  : <span
+                    onKeyDown={revokeMainNewsletterConsent}
+                  >
+                    abbestellen
+                  </span>
+                ) : (
+                  <span
                     aria-hidden="true"
                     className={gS.linkLikeFormated}
                     onClick={revokeMainNewsletterConsent}
-                    onKeyDown={revokeMainNewsletterConsent}>Newsletter erhalten</span>
-                }
+                    onKeyDown={revokeMainNewsletterConsent}
+                  >
+                    Newsletter erhalten
+                  </span>
+                )}
               </p>
             </div>
             {/* Conditionally render individual Cards */}
             <div>{activeNewsletterCards}</div>
-          </section> : null
-        }
+          </section>
+        ) : null}
         <p className={gS.linkLikeFormated}>Alle abbestellen</p>
 
         <h4 className={gS.optionSectionHeading}>Newsletter hinzufügen</h4>
-        <SearchPlaces showButton={false} />
+        <SearchPlaces showButton={false} onPlaceSelect={handlePlaceSelect} />
 
-        <p onClick={() => addNewsletter(newNewsletter)} className={gS.linkLikeFormated}>Test: Newsletter hinzufügen!</p>
+        <p
+          onClick={() => addNewsletter(newNewsletter)}
+          className={gS.linkLikeFormated}
+        >
+          Test: Newsletter hinzufügen!
+        </p>
 
         <h4 className={gS.optionSectionHeading}>Kontakt per Telefon</h4>
         <p className={s.newsletterCardDescription}>
           Hier kannst du angeben, ob wir dich auch telefonisch erreichen können.
           Eine Telefonnummer erleichtert es uns, dich für die Koordination von
           Veranstaltungen zu erreichen.
-          <br /><br />
+          <br />
+          <br />
           Mit dem Eintragen stimmst du zu, dass wir dich kontaktieren dürfen.
         </p>
         <div className={s.optionRow}>
@@ -119,10 +149,11 @@ export default ({ userData, userId }) => {
 
         <h4 className={gS.optionSectionHeading}>Kontakt per Messenger</h4>
         <p className={s.newsletterCardDescription}>
-          Wenn du möchtest, kannst du auch per Messenger mit uns Kontakt aufnehmen:
+          Wenn du möchtest, kannst du auch per Messenger mit uns Kontakt
+          aufnehmen:
         </p>
         <MessengerButtonRow iconSize="XL" />
       </section>
-    </section >
-  )
+    </section>
+  );
 };
