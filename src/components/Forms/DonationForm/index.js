cbdd6f9bdaf8e3edf80c51840bee0ee6e87a5f1f
felixbuchholz <felixbuchholz@gmx.net>
2020-12-11 13:19:10 +0100

    style back to start page fix

--- src/components/Forms/DonationForm/index.js
+++ src/components/Forms/DonationForm/index.js
@@ -334,6 +334,11 @@ export default theme => {
                           >
                             Lieber einmalig spenden
                           </Link>
+
+                          <p className={s.hint}>
+                            Hinweis: Du kannst deine monatliche Spende jederzeit
+                            wieder beenden.
+                          </p>
                         </div>
                       )}
 
