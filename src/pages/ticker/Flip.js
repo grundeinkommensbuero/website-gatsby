14bb1a964cf1fefd250842c58ddf2171eecca7b4
nmauersberg <n.mauersberg@gmail.com>
2021-01-14 13:08:39 +0100

    Preview of ticker variants

--- src/pages/ticker/Flip.js
+++ src/pages/ticker/Flip.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Tick from "@pqina/flip";
-import "@pqina/flip/dist/flip.min.css";
+import "./flip.min.css";
 
 export default class Flip extends React.Component {
   constructor(props) {
--- src/pages/ticker/index.js
+++ src/pages/ticker/index.js
@@ -3,27 +3,59 @@ import React, { useEffect, useState } from 'react';
 import Layout from '../../components/Layout';
 import { Button } from '../../components/Forms/Button';
 import Flip from "./Flip";
+import Reel from 'react-reel'
+
+import { CountUp } from 'use-count-up';
 
 import s from './style.module.less';
 import cN from 'classnames';
+
+import './reelstyle.css';
 
 const App = () => {
 
   const [peopleCount, setPeopleCount] = useState(3592);
   const [municipalityCount, setMunicipalityCount] = useState(43);
 
+  const [reelCounterText, setReelCounterText] = useState();
+  const [reelCounterMunicipalityText, setReelCounterMunicipalityText] = useState();
+
   useEffect(() => {
     setTimeout(() => {
       setPeopleCount(peopleCount + 1);
-    }, 250);
+    }, 1000);
 
     setTimeout(() => {
       setMunicipalityCount(municipalityCount + 1);
     }, 5000);
   });
 
+  useEffect(() => {
+    setReelCounterText(reelCounterText + 'Menschen');
+    setReelCounterMunicipalityText(reelCounterMunicipalityText + 'Orten');
+  }, [peopleCount, municipalityCount]);
+
   return (
     <Layout>
+      <section className={s.contentContainer}>
+        <div className={s.App}>
+          <div className={s.counterContainer}>
+            <Reel text={peopleCount.toString()} />
+            <h1 className={s.counterLabelThird}>Menschen</h1>
+          </div>
+          <div className={cN(s.counterContainer, s.alignRight)}>
+            <h1 className={s.counterLabelThird}>in</h1>
+            <Reel text={municipalityCount.toString()} />
+            <h1 className={s.counterLabelThird}>Orten</h1>
+          </div>
+          <h1>
+            lassen das Grundeinkommen in Deutschland Realität werden.
+          </h1>
+          <Button className={s.CTA}>Sei dabei.</Button>
+        </div>
+      </section>
+
+
       <section className={s.contentContainer}>
         <div className={s.App}>
           <div className={s.counterContainer}>
@@ -41,6 +73,30 @@ const App = () => {
           <Button className={s.CTA}>Sei dabei.</Button>
         </div>
       </section>
+
+      <section className={s.contentContainer}>
+        <div className={s.App}>
+          <div className={s.counterContainer}>
+            <div className={cN(s.upperCounter, s.countUpLibNum)} >
+              <CountUp isCounting end={peopleCount} start={3000} duration={10} />
+            </div>
+            <h1 className={s.counterLabelSecond}>Menschen</h1>
+          </div>
+          <div className={cN(s.counterContainer, s.alignRight)}>
+            <h1 className={s.counterLabelSecond}>in</h1>
+            <div className={s.countUpLibNum}>
+              <CountUp isCounting end={municipalityCount} start={30} duration={10} />
+            </div>
+            <h1 className={s.counterLabelSecond}>Orten</h1>
+          </div>
+          <h1>
+            lassen das Grundeinkommen in Deutschland Realität werden.
+          </h1>
+          <Button className={s.CTA}>Sei dabei.</Button>
+        </div>
+      </section>
+
+      <br /><br /><br /><br /><br /><br />
     </Layout>
   )
 }
