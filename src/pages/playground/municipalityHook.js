8412d0701f126ed722cc94f4f6d3c10b87e78d02
nmauersberg <n.mauersberg@gmail.com>
2021-02-11 13:38:28 +0100

    Minifix on playground

--- src/pages/playground/municipalityHook.js
+++ src/pages/playground/municipalityHook.js
@@ -11,20 +11,14 @@ import { useGetMunicipalityStats } from '../../hooks/Api/Municipalities';
 export default () => {
   const [state1, stats1, getMunicipalityStats1] = useGetMunicipalityStats();
   const [state2, stats2, getMunicipalityStats2] = useGetMunicipalityStats();
-  const [
-    /*state1, stats1, */ getMunicipalityStats1,
-  ] = useGetMunicipalityStats();
-  const [
-    /*state2, stats2, */ getMunicipalityStats2,
-  ] = useGetMunicipalityStats();
 
   useEffect(() => {
     getMunicipalityStats1();
     getMunicipalityStats2('091088');
   }, []);
 
-  // console.log({ stats1 }, { state1 });
-  // console.log({ stats2 }, { state2 });
+  console.log({ stats1 }, { state1 });
+  console.log({ stats2 }, { state2 });
 
   return (
     <Layout>
