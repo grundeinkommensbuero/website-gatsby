e762e0e71baf527efc8fc382023805ff6aadaef4
nmauersberg <n.mauersberg@gmail.com>
2021-02-17 11:31:03 +0100

    Little stylefix

--- src/components/Onboarding/Anmeldung/index.js
+++ src/components/Onboarding/Anmeldung/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import gS from '../style.module.less';
+import s from './style.module.less';
 import SignUp from '../../Forms/SignUp';
 
 
@@ -7,12 +8,12 @@ export const Anmeldung = ({ userData, compIndex, setCurrentElementByIndex }) => 
 
   return (
     <section className={gS.pageContainer}>
-
-      <SignUp
-        illustration={false}
-        showSignedInMessage={false}
-      />
-
+      <div className={s.signupContainer}>
+        <SignUp
+          illustration={false}
+          showSignedInMessage={false}
+        />
+      </div>
     </section>
   );
 };
