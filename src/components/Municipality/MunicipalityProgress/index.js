5bd96feea170a7ba096ca7c10193877bfd76979d
felixbuchholz <felixbuchholz@gmx.net>
2021-02-17 19:19:25 +0100

    adds progress bar fix

--- src/components/Municipality/MunicipalityProgress/index.js
+++ src/components/Municipality/MunicipalityProgress/index.js
@@ -4,45 +4,50 @@ import { MunicipalityContext } from '../../../context/Municipality';
 
 export const MunicipalityProgress = () => {
   const { municipality } = useContext(MunicipalityContext);
-  const statsReady = municipality?.signups && municipality?.goal > 0;
-  return (
-    <>
-      {statsReady && (
-        <div>
-          <h2>
-            Gemeinsam bringen wir das Grundeinkommen nach {municipality.name}!
-          </h2>
-          <CampainVisualisation
-            goal={municipality.goal}
-            count={municipality.signups}
-            // showCTA={visualisations.length !== 1 && visualisation.ctaLink}
-            labels={{
-              NEEDED: () => <>Benötigte Anmeldungen</>,
-              GOAL_INBETWEEN_TOOLTIP: count => (
-                <>
-                  Insgesamt benötigt:
-                  <br />
-                  {count} Anmeldungen
-                </>
-              ),
-              GOAL_INBETWEEN: count => <>Nächstes Ziel: {count} Anmeldungen</>,
-              CURRENT_COUNT: () => <>Anmeldungen</>,
-              CTA: () => <>Mitmachen</>,
-            }}
-            currency="Anmeldungen"
-            startDate={new Date()}
-          />
-          <h3>{getSignupsLabel(municipality.signups, municipality.goal)}</h3>
-          <p>
-            Wenn wir ingesamt {municipality.goal} Menschen sind, dann trauen wir
-            uns zu, gemeinsam den offiziellen Bürgerbegehrens-prozess für
-            unseren Modellversuch zu starten. Hierfür müssen wir möglichst viele
-            sein. Hol also noch Menschen dazu!
-          </p>
-        </div>
-      )}
-    </>
-  );
+  const statsReady = municipality?.goal > 0;
+  let signups = municipality?.signups || 0;
+  if (typeof signups !== 'number') {
+    signups = 0;
+  }
+
+  if (statsReady) {
+    return (
+      <div>
+        <h2>
+          Gemeinsam bringen wir das Grundeinkommen nach {municipality.name}!
+        </h2>
+        <CampainVisualisation
+          goal={municipality.goal}
+          count={signups}
+          // showCTA={visualisations.length !== 1 && visualisation.ctaLink}
+          labels={{
+            NEEDED: () => <>Benötigte Anmeldungen</>,
+            GOAL_INBETWEEN_TOOLTIP: count => (
+              <>
+                Insgesamt benötigt:
+                <br />
+                {count} Anmeldungen
+              </>
+            ),
+            GOAL_INBETWEEN: count => <>Nächstes Ziel: {count} Anmeldungen</>,
+            CURRENT_COUNT: () => <>Anmeldungen</>,
+            CTA: () => <>Mitmachen</>,
+          }}
+          currency="Anmeldungen"
+          startDate={false}
+        />
+        <h3>{getSignupsLabel(municipality.signups, municipality.goal)}</h3>
+        <p>
+          Wenn wir ingesamt {municipality.goal} Menschen sind, dann trauen wir
+          uns zu, gemeinsam den offiziellen Bürgerbegehrens-prozess für unseren
+          Modellversuch zu starten. Hierfür müssen wir möglichst viele sein. Hol
+          also noch Menschen dazu!
+        </p>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 const getSignupsLabel = (signups, goal) => {
