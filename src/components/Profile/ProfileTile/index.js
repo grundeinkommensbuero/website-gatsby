75d3be9b682c9f232d7a550333e8c669e274f897
felixbuchholz <felixbuchholz@gmx.net>
2021-02-17 10:59:39 +0100

    basic profile tile setup

--- src/components/Profile/ProfileTile/index.js
+++ src/components/Profile/ProfileTile/index.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import AuthContext from '../../../context/Authentication';
 import { MunicipalityContext } from '../../../context/Municipality';
+import { useUserMunicipalityContentfulState } from '../../../hooks/Municipality/UserMunicipalityContentfulState';
 import AvatarImage from '../../AvatarImage';
 import { LinkButtonLocal } from '../../Forms/Button';
 import { formatDate } from '../../utils';
@@ -10,7 +11,27 @@ import cN from 'classnames';
 export const ProfileTile = ({ body }) => {
   const { userId, customUserData: userData } = useContext(AuthContext);
   const { municipality } = useContext(MunicipalityContext);
+  const tileData = { userId, userData, municipality };
 
+  const {
+    municipalityContentfulState,
+    userContentfulState,
+  } = useUserMunicipalityContentfulState();
+  console.log(userContentfulState, municipalityContentfulState);
+
+  if (userContentfulState === 'loggedInThisMunicipalitySignup') {
+    return <TileLoggedInThisMunicipality {...tileData} />;
+  }
+  if (
+    municipalityContentfulState === 'noMunicipality' &&
+    userContentfulState === 'loggedInOtherMunicipalitySignup'
+  ) {
+    return <TileLoggedInOtherMunicipality {...tileData} />;
+  }
+  return <></>;
+};
+
+const TileLoggedInThisMunicipality = ({ userId, userData, municipality }) => {
   return (
     <>
       {userId && (
@@ -32,11 +53,6 @@ export const ProfileTile = ({ body }) => {
                     formatDate(new Date(userData.createdAt))}
                 </li>
               </ul>
-              <div>
-                <LinkButtonLocal to={`/mensch/${userId}`}>
-                  Zum Profil
-                </LinkButtonLocal>
-              </div>
             </div>
           </div>
           <div className={s.flexElement}>
@@ -48,6 +64,59 @@ export const ProfileTile = ({ body }) => {
               </p>
             )}
             <p>Besuche dein Profil, um deine Einstellungen zu ändern.</p>
+            <div>
+              <LinkButtonLocal to={`/mensch/${userId}`}>
+                Zum Profil
+              </LinkButtonLocal>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+const TileLoggedInOtherMunicipality = ({ userId, userData, municipality }) => {
+  console.log(userData);
+
+  return (
+    <>
+      {userId && (
+        <div className={cN(s.tileContainer, s.sectionWhite)}>
+          <div className={s.flexElement}>
+            <h3 className={s.headline}>Hallo {userData.username}! </h3>
+
+            <p>Du bist bereits in folgenden Orten angemeldet:</p>
+
+            {userData.municipalities.map(municipality => {
+              return <div>{municipality.name}</div>;
+            })}
+
+            <p>Besuche dein Profil, um deine Einstellungen zu ändern.</p>
+            <div>
+              <LinkButtonLocal to={`/mensch/${userId}`}>
+                Zum Profil
+              </LinkButtonLocal>
+            </div>
+          </div>
+          <div className={s.avatarAndInfo}>
+            <div className={s.avatarContainer}>
+              <AvatarImage user={userData} className={s.avatar} />
+            </div>
+
+            <div className={s.info}>
+              <h3 className={cN(s.headline, s.centerMobile)}>
+                {userData.username}
+              </h3>
+              <ul className={cN(s.infoText, s.centerMobile)}>
+                <li className={s.centerMobile}>{userData.city}</li>
+                <li className={s.centerMobile}>
+                  Dabei seit dem{' '}
+                  {userData.createdAt &&
+                    formatDate(new Date(userData.createdAt))}
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       )}
