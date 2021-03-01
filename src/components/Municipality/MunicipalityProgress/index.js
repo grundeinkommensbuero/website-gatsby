1ff37d9ccc1c9ad82c18a09e4387ee639215e071
joyponader <joyponader@gmail.com>
2021-03-01 01:59:51 +0100

    Update index.js

--- src/components/Municipality/MunicipalityProgress/index.js
+++ src/components/Municipality/MunicipalityProgress/index.js
@@ -42,9 +42,18 @@ export const MunicipalityProgress = ({
           startDate={false}
         />
         <h3>{getSignupsLabel(municipality.signups, municipality.goal)}</h3>
-        {showDescription && (
+        //Berlin Hamburg Bremen
+        {showDescription && (municipality.ags === '11000000' || municipality.ags === '02000000' || municipality.ags === '04011000') && (
           <p>
-            Um den offiziellen {municipality.ags === '11000000' ? 'Volksbegehrensprozess in die zweite, entscheidende Runde zu bringen' : 'Bürgerbegehrensprozess zu starten'},
+            Um den offiziellen Volksbegehrensprozess in die zweite, entscheidende Runde zu bringen,
+            brauchen wir mindestens {municipality.goal} Menschen aus {municipality.name}. 
+            Hol also noch Menschen dazu!
+          </p>
+        )}
+        //all other municipalities
+        {showDescription && !(municipality.ags === '11000000' || municipality.ags === '02000000' || municipality.ags === '04011000') && (
+          <p>
+            Um den offiziellen Bürgerbegehrensprozess zu starten,
             brauchen wir mindestens {municipality.goal} Menschen aus {municipality.name}. 
             Hol also noch Menschen dazu!
           </p>
@@ -61,13 +70,28 @@ export const MunicipalityProgress = ({
           </h2>
         )}
         {/* <h3>{getSignupsLabel(municipality.signups, municipality.goal)}</h3> */}
-        {showDescription && (
+        {showDescription && (municipality.ags === '11000000' || municipality.ags === '02000000' || municipality.ags === '04011000') && (
           <>
             <p>
-              Wenn wir ingesamt {municipality.goal} Menschen sind, dann trauen
-              wir uns zu, gemeinsam den offiziellen Bürgerbegehrensprozess für
-              unseren Modellversuch zu starten. Hierfür müssen wir möglichst
-              viele sein. Hol also noch Menschen dazu!
+              Um den offiziellen Volksbegehrensprozess in die zweite, entscheidende Runde zu bringen,
+              brauchen wir mindestens {municipality.goal} Menschen aus {municipality.name}. 
+              Hol also noch Menschen dazu!
+            </p>
+            <p className={s.returnHint}>
+              Derzeit scheint unsere Datenbank etwas überlastet und wir können
+              dir momentan leider nicht sagen, wieviele Menschen sich schon
+              {municipality ? `in ${municipality.name}` : ''} angemeldet haben.
+              In einigen Minuten solltest du hier wieder die aktuellsten Infos{' '}
+              {municipality ? `zu ${municipality.name}` : ''} sehen.
+            </p>
+          </>
+        )}
+        {showDescription && !(municipality.ags === '11000000' || municipality.ags === '02000000' || municipality.ags === '04011000') && (
+          <>
+            <p>
+              Um den offiziellen Bürgerbegehrensprozess zu starten,
+              brauchen wir mindestens {municipality.goal} Menschen aus {municipality.name}. 
+              Hol also noch Menschen dazu!
             </p>
             <p className={s.returnHint}>
               Derzeit scheint unsere Datenbank etwas überlastet und wir können
