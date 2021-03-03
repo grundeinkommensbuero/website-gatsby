285547596845c1fdbb9b325d5ea64b3f16db9b21
Nicolai Mauersberg <74594947+nmauersberg@users.noreply.github.com>
2021-03-03 17:46:10 +0100

    Implements 60 second code resend timer (#259)
    * Implements 60 second code resend timer
    * Enhance trigger for timer function

--- src/components/Login/EnterLoginCode/index.js
+++ src/components/Login/EnterLoginCode/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { navigate } from 'gatsby';
 import { Form, Field } from 'react-final-form';
 
@@ -26,6 +26,8 @@ export const EnterLoginCode = ({
     setAnswerChallengeState,
   ] = useAnswerChallenge();
   const [signInState, startSignIn] = useSignIn();
+  const [triggerMinuteTimer, setTriggerOneMinuteTimer] = useState(0);
+  const [timerCounter, setTimerCounter] = useState(0);
 
   useEffect(() => {
     // We don't want to start sign in again, if flag is set
@@ -47,6 +49,22 @@ export const EnterLoginCode = ({
       startSignIn();
     }
   }, [answerChallengeState, isAuthenticated]);
+
+  useEffect(() => {
+    countdown();
+  }, [triggerMinuteTimer]);
+
+  const countdown = () => {
+    let seconds = 60;
+    function tick() {
+      seconds--;
+      setTimerCounter(seconds);
+      if (seconds > 0) {
+        setTimeout(tick, 1000);
+      }
+    }
+    tick();
+  };
 
   if (answerChallengeState === 'loading' || signInState === 'loading') {
     return (
@@ -164,15 +182,22 @@ export const EnterLoginCode = ({
                   <CTAButton type="submit">
                     {buttonText ? buttonText : 'Abschicken'}
                   </CTAButton>
-                  <InlineButton
+                  {timerCounter === 0 ? <InlineButton
                     type="button"
                     onClick={() => {
                       setAnswerChallengeState(undefined);
                       setCode('resendCode');
+                      setTriggerOneMinuteTimer(triggerMinuteTimer + 1);
                     }}
                   >
                     Code erneut senden
-                  </InlineButton>
+                  </InlineButton> :
+                    <div className={s.counterDescriptionContainer}>
+                      <p className={s.counterDescription}>
+                        In {timerCounter} {timerCounter !== 1 ? 'Sekunden' : 'Sekunde'} kannst du{' '}
+                        den Code erneut anfordern
+                      </p>
+                    </div>}
                 </CTAButtonContainer>
               </form>
             </FormWrapper>
