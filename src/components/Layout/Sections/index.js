27dce9cf0718a9f425e96e00539ace140ce89406
nmauersberg <n.mauersberg@gmail.com>
2021-03-12 15:00:38 +0100

    Loading Animation hotfix

--- src/components/Layout/Sections/index.js
+++ src/components/Layout/Sections/index.js
@@ -370,7 +370,7 @@ export function ContentfulSection({ section, pageContext }) {
           <DonationForm theme={theme}></DonationForm>
         </SectionInner>
       )}
-      {isSharingFeature && userData.municipalities ? (
+      {isSharingFeature && userData.municipalities && (
         <>
           <div ref={scrollToRef}></div>
           <SectionInner>
@@ -406,7 +406,7 @@ export function ContentfulSection({ section, pageContext }) {
             }
           </SectionInner>
         </>
-      ) : <LoadingAnimation />}
+      )}
       {(body || pledgeId || signaturesId) && (
         <SectionInner hugeText={bodyTextSizeHuge}>
           {body && body.json ? contentfulJsonToHtml(body.json) : body}
