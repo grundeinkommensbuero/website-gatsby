e402f62ca73292579d79ae0e6100193a218956a6
nmauersberg <n.mauersberg@gmail.com>
2021-03-22 15:56:22 +0100

    Updates sticky banner text and margin

--- src/components/Layout/Header/StickyDonationBar/index.jsx
+++ src/components/Layout/Header/StickyDonationBar/index.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import s from './style.module.less';
 
 export const StickyDonationBar = className => {
-  const closeIcon = require('./close-icon.svg');
+  // const closeIcon = require('./close-icon.svg');
 
   return (
     <div className={s.donationBar}>
       <div className={s.donationBarItemContainer}>
         <p className={s.donationBarItem}>
           Schon 16 Orte haben sich qualifiziert, ganz viele sind auf dem Weg zum
-          Ziel. Noch bis Sonntag anmelden!
+          Ziel. Hilf mit weitere Orte ins Ziel zu bringen!
         </p>
         {/* <button className={s.donationBarCTA}>Sei dabei</button>
         <img
