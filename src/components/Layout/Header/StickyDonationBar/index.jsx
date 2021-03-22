51ce0df0ed8e21e152449b759754b17afe80bb5d
nmauersberg <n.mauersberg@gmail.com>
2021-03-22 11:12:07 +0100

    Updates sticky banner municipality count

--- src/components/Layout/Header/StickyDonationBar/index.jsx
+++ src/components/Layout/Header/StickyDonationBar/index.jsx
@@ -8,7 +8,7 @@ export const StickyDonationBar = className => {
     <div className={s.donationBar}>
       <div className={s.donationBarItemContainer}>
         <p className={s.donationBarItem}>
-          Schon 12 Orte haben sich qualifiziert, ganz viele sind auf dem Weg zum
+          Schon 16 Orte haben sich qualifiziert, ganz viele sind auf dem Weg zum
           Ziel. Noch bis Sonntag anmelden!
         </p>
         {/* <button className={s.donationBarCTA}>Sei dabei</button>
