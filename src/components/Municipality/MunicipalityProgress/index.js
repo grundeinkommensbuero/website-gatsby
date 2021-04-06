b90e0b39c6d3bdcda23bdae87fc30ee84a6c76bc
Tatiana Schär <tsc@spacesquad.de>
2021-04-06 12:22:09 +0200

    Added map for Bremerhaven

--- src/components/Municipality/MunicipalityProgress/index.js
+++ src/components/Municipality/MunicipalityProgress/index.js
@@ -62,14 +62,23 @@ export const MunicipalityProgress = ({
           <>
             <h2 className={s.collectionMapHeadline}>Vor Ort unterschreiben</h2>
             <p>
-              Hier findest du Orte in Bremen, an denen unsere Listen ausliegen.
-              Komm einfach vorbei und unterschreibe vor Ort!
+              Hier findest du Orte in Bremen und Bremerhaven, an denen unsere
+              Listen ausliegen. Komm einfach vorbei und unterschreibe vor Ort!
             </p>
             <Map
               mapConfig={{
                 state: 'bremen',
                 config: {
                   center: [8.8108603, 53.0782442],
+                  zoom: 11,
+                },
+              }}
+            />
+            <Map
+              mapConfig={{
+                state: 'bremen',
+                config: {
+                  center: [8.6069175, 53.5480746],
                   zoom: 11,
                 },
               }}
