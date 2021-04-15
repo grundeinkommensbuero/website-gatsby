856662c5156e8e3c1c7a9a2366ee09567a55c08e
nmauersberg <n.mauersberg@gmail.com>
2021-04-15 12:54:57 +0200

    Map and Leaderboard positioning and style

--- src/components/Municipality/MunicipalityMapAndSearch/index.js
+++ src/components/Municipality/MunicipalityMapAndSearch/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Legend, MunicipalityMap } from '../MunicipalityMap';
-import { MunicipalitySearch } from '../MunicipalitySearch';
+import { MunicipalityMap } from '../MunicipalityMap';
+import { WrappedMunicipalitySearch } from '../MunicipalitySearch/WrappedMunicipalitySearch';
 import { Leaderboard } from '../Leaderboard';
 
 import s from './style.module.less';
@@ -8,26 +8,20 @@ import s from './style.module.less';
 export const MunicipalityMapAndSearch = () => {
   return (
     <>
-
       <div className={s.mapSectionContainer}>
         <div className={s.map}>
+          <div className={s.hideOnDesktop}>
+            <WrappedMunicipalitySearch />
+          </div>
           <MunicipalityMap />
         </div>
         <div className={s.mapControlContainer}>
-          <div className={s.searchWrapper}>
-            <p className={s.searchInfoText}>
-              Finde deinen Ort und schaue, wie viele Menschen schon dabei sind:
-        </p>
-            <MunicipalitySearch />
+          <div className={s.hideOnMobile}>
+            <WrappedMunicipalitySearch />
           </div>
-          <div className={s.topDesktopLegend}>
+          <div className={s.leaderboard}>
             <Leaderboard />
-            {/* <Legend /> */}
           </div>
-        </div>
-        <div className={s.bottomMobileLegend}>
-          <Leaderboard />
-          {/* <Legend /> */}
         </div>
       </div>
     </>
