4b702cfca0669237a1b1ff30757ca659e40c5b4f
nmauersberg <n.mauersberg@gmail.com>
2021-05-10 14:27:57 +0200

    Update crowndfunding next target manually

--- src/components/CrowdfundingInfo/CrowdfundingVisual.js
+++ src/components/CrowdfundingInfo/CrowdfundingVisual.js
@@ -1,21 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { CampainVisualisation } from '../CampaignVisualisations';
 import { useGetCrowdfundingDirectly } from '../../hooks/Api/Crowdfunding';
 
 export const CrowdfundingVisual = () => {
   const [crowdFunding] = useGetCrowdfundingDirectly(109676);
-  const [nextTarget, setNextTarget] = useState(0);
-  useEffect(() => {
-    if (crowdFunding) {
-      // Set target to next full 5000€
-      setNextTarget(Math.ceil(crowdFunding.project.funding_status / 5000) * 5000);
-    }
-  }, [crowdFunding]);
 
   return (
     <div>
       {crowdFunding && <CampainVisualisation
-        goal={nextTarget}
+        goal={Math.round(14400)}
         count={Math.round(crowdFunding.project.funding_status)}
         isCrowdfunding={true}
         // showCTA={visualisations.length !== 1 && visualisation.ctaLink}
