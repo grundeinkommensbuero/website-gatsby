f72b6b58b4ee7551cbc35ad822e75f8acfae3f4d
nmauersberg <n.mauersberg@gmail.com>
2021-05-10 14:24:59 +0200

    Update crowdfunding target in banner

--- src/components/CrowdfundingInfo/CrowdfundingVisual.js
+++ src/components/CrowdfundingInfo/CrowdfundingVisual.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { CampainVisualisation } from '../CampaignVisualisations';
 import { useGetCrowdfundingDirectly } from '../../hooks/Api/Crowdfunding';
 
 export const CrowdfundingVisual = () => {
   const [crowdFunding] = useGetCrowdfundingDirectly(109676);
-  // console.log(crowdFunding);
+  const [nextTarget, setNextTarget] = useState(0);
+  useEffect(() => {
+    if (crowdFunding) {
+      // Set target to next full 5000€
+      setNextTarget(Math.ceil(crowdFunding.project.funding_status / 5000) * 5000);
+    }
+  }, [crowdFunding]);
 
   return (
     <div>
       {crowdFunding && <CampainVisualisation
-        goal={Math.round(crowdFunding.project.funding_target)}
+        goal={nextTarget}
         count={Math.round(crowdFunding.project.funding_status)}
         isCrowdfunding={true}
         // showCTA={visualisations.length !== 1 && visualisation.ctaLink}
