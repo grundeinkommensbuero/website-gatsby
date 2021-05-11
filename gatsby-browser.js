3a0eae13e9e42dd7e41c9b16763681e22d407bf2
Valentin Schagerl <Vali_Schagerl@web.de>
2021-05-11 11:27:30 +0200

    Workaround for anchors not working on page load (#306)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,3 +12,46 @@ if (location.host.indexOf('localhost') !== 0) {
     application: 'expedition-grundeinkommen',
   });
 }
+
+// Workaroud for hashes not working (https://github.com/gatsbyjs/gatsby/issues/386#issuecomment-249463446)
+export const onRouteUpdate = ({ location }) => {
+  jumpToHash(location.hash);
+};
+
+// We check every x ms if the element of the hash exists and then scroll to it.
+// This is useful for slow internet.
+const jumpToHash = hash => {
+  if (hash) {
+    waitForElementToDisplay(
+      hash,
+      element => {
+        element.scrollIntoView();
+      },
+      800,
+      15000
+    );
+  }
+};
+
+// Function to wait for the display of an element
+const waitForElementToDisplay = (
+  selector,
+  callback,
+  checkFrequencyInMs,
+  timeoutInMs
+) => {
+  var startTimeInMs = Date.now();
+  (function loopSearch() {
+    const element = document.querySelector(selector);
+    console.log('element', element);
+    if (element !== null) {
+      callback(element);
+      return;
+    } else {
+      setTimeout(function() {
+        if (timeoutInMs && Date.now() - startTimeInMs > timeoutInMs) return;
+        loopSearch();
+      }, checkFrequencyInMs);
+    }
+  })();
+};
