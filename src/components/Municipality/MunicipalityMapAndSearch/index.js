484f3aa572a1e3722b15561e351d15d35083c774
Valioesi <Vali_Schagerl@web.de>
2021-05-18 17:55:21 +0200

    React.lazy load municipality map and search

--- src/components/Municipality/MunicipalityMapAndSearch/index.js
+++ src/components/Municipality/MunicipalityMapAndSearch/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { MunicipalityMap } from '../MunicipalityMap';
+import { FallbackMap } from '../MunicipalityMap';
 import { WrappedMunicipalitySearch } from '../MunicipalitySearch/WrappedMunicipalitySearch';
 import { Leaderboard } from '../Leaderboard';
-
 import * as s from './style.module.less';
 
+const MunicipalityMap = React.lazy(() => import('../MunicipalityMap'));
+
 export const MunicipalityMapAndSearch = () => {
+  const isSSR = typeof window === 'undefined';
+
   return (
     <>
       <div className={s.mapSectionContainer}>
@@ -13,7 +16,13 @@ export const MunicipalityMapAndSearch = () => {
           <div className={s.hideOnDesktop}>
             <WrappedMunicipalitySearch />
           </div>
-          <MunicipalityMap />
+          {isSSR ? (
+            <FallbackMap />
+          ) : (
+            <React.Suspense fallback={<FallbackMap />}>
+              <MunicipalityMap />
+            </React.Suspense>
+          )}
         </div>
         <div className={s.mapControlContainer}>
           <div className={s.hideOnMobile}>
--- src/components/Municipality/MunicipalitySearch/WrappedMunicipalitySearch.js
+++ src/components/Municipality/MunicipalitySearch/WrappedMunicipalitySearch.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { MunicipalitySearch } from './index';
 import * as s from './style.module.less';
 
+const MunicipalitySearch = React.lazy(() => import('./index'));
+
 export const WrappedMunicipalitySearch = ({ searchTitle }) => {
+  const isSSR = typeof window === 'undefined';
+
   return (
     <div className={s.searchWrapper}>
       <p className={s.searchInfoText}>
         Finde deinen Ort und schaue, wie viele Menschen schon dabei sind:
       </p>
-      <MunicipalitySearch />
+      {isSSR ? (
+        <div>Lädt...</div>
+      ) : (
+        <React.Suspense fallback={<div>Lädt...</div>}>
+          <MunicipalitySearch />
+        </React.Suspense>
+      )}
     </div>
   );
 };
