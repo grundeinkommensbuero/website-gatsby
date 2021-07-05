c7cc6485b03940243c0468acfdda416a4b8fe204
Valioesi <Vali_Schagerl@web.de>
2021-07-05 10:37:35 +0200

    Don't show banner

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -61,7 +61,7 @@ const Header = ({ menu, stickyBannerVisible }) => {
             )}
           </>
         </div>
-        {stickyBannerVisible && <HertieCrowdfunding />}
+        {/* {stickyBannerVisible && <HertieCrowdfunding />} */}
       </header>
     </>
   );
