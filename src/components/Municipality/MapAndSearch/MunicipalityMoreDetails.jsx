2fbd36e16ac6260ab7699899cb25401b27a993a3
nmauersberg <n.mauersberg@gmail.com>
2021-07-26 13:35:03 +0200

    Fix CampaignVisualisation

--- src/components/Municipality/MapAndSearch/MunicipalityMoreDetails.jsx
+++ src/components/Municipality/MapAndSearch/MunicipalityMoreDetails.jsx
@@ -6,26 +6,28 @@ import * as s from './style.module.less';
 export const MunicipalityMoreDetails = ({ municipality }) => {
   return (
     <section className={s.expandedRow}>
-      <CampainVisualisation
-        goal={municipality.goal}
-        count={municipality.signups}
-        onWhiteBackground={true}
-        // showCTA={visualisations.length !== 1 && visualisation.ctaLink}
-        labels={{
-          NEEDED: () => <>Benötigte Anmeldungen</>,
-          GOAL_INBETWEEN_TOOLTIP: count => (
-            <>
-              Insgesamt benötigt:
-              <br />
-              {count} Anmeldungen
-            </>
-          ),
-          GOAL_INBETWEEN: count => <>Nächstes Ziel: {count} Anmeldungen</>,
-          CURRENT_COUNT: () => <>Anmeldungen</>,
-          CTA: () => <>Mitmachen</>,
-        }}
-        currency="Anmeldungen"
-      />
+      {municipality?.goal && (
+        <CampainVisualisation
+          goal={municipality.goal}
+          count={municipality.signups}
+          onWhiteBackground={true}
+          // showCTA={visualisations.length !== 1 && visualisation.ctaLink}
+          labels={{
+            NEEDED: () => <>Benötigte Anmeldungen</>,
+            GOAL_INBETWEEN_TOOLTIP: count => (
+              <>
+                Insgesamt benötigt:
+                <br />
+                {count} Anmeldungen
+              </>
+            ),
+            GOAL_INBETWEEN: count => <>Nächstes Ziel: {count} Anmeldungen</>,
+            CURRENT_COUNT: () => <>Anmeldungen</>,
+            CTA: () => <>Mitmachen</>,
+          }}
+          currency="Anmeldungen"
+        />
+      )}
       <div className={s.buttonContainer}>
         <Button className={s.municipalityButton}>Zur Ortsseite</Button>
       </div>
