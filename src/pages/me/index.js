2e6b06e58b1be7ad012fd4808c7c1be51182494c
Valioesi <Vali_Schagerl@web.de>
2021-08-26 12:37:46 +0200

    Added /me page to redirect to /mensch/:userId

---
+++ src/pages/me/index.js
@@ -0,0 +1,44 @@
+import { navigate } from 'gatsby';
+import React, { useContext, useEffect } from 'react';
+import {
+  Section,
+  SectionInner,
+  SectionWrapper,
+} from '../../components/Layout/Sections';
+import AuthContext from '../../context/Authentication';
+import Layout from '../../components/Layout';
+import { FinallyMessage } from '../../components/Forms/FinallyMessage';
+import { useLocation } from '@reach/router';
+
+const MePage = () => {
+  const { userId } = useContext(AuthContext);
+  const location = useLocation();
+
+  useEffect(() => {
+    // Get path after me/ to redirect to a specifc subpage of profile
+    const splitPath = location.pathname.split('me/');
+    const profilePath =
+      splitPath.length <= 1 ? '' : splitPath[splitPath.length - 1];
+
+    console.log('profilePath ', profilePath);
+    if (userId) {
+      navigate(`/mensch/${userId}/${profilePath}`);
+    } else {
+      navigate(`/login/?nextPage=me%2F${profilePath}`);
+    }
+  }, [userId]);
+
+  return (
+    <Layout>
+      <SectionWrapper>
+        <Section>
+          <SectionInner>
+            <FinallyMessage type="progress">Lade...</FinallyMessage>
+          </SectionInner>
+        </Section>
+      </SectionWrapper>
+    </Layout>
+  );
+};
+
+export default MePage;
