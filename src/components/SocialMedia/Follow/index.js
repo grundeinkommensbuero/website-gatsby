f5829cc04e544f2a9da4326daf85091805276c8a
Tatiana Schär <tatiana.schaer@hotmail.com>
2021-09-21 17:08:59 +0200

    Changed footer icons

--- src/components/SocialMedia/Follow/index.js
+++ src/components/SocialMedia/Follow/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as s from './style.module.less';
 import SocialMediaButton from '../Button';
 
 export default ({ className }) => (
@@ -8,19 +7,16 @@ export default ({ className }) => (
       icon="Facebook"
       link="https://www.facebook.com/expedition.grundeinkommen"
       label="Folge auf Facebook"
-      className={s.button}
     />
     <SocialMediaButton
       icon="Twitter"
       link="https://twitter.com/expeditionbge"
       label="Folge auf Twitter"
-      className={s.button}
     />
     <SocialMediaButton
       icon="Instagram"
       link="https://www.instagram.com/expedition.grundeinkommen/"
       label="Folge auf Instagram"
-      className={s.button}
     />
   </div>
 );
