bca2f1eb13950ed1c55fcdb477b3b88ccb09dba0
nmauersberg <n.mauersberg@gmail.com>
2021-10-19 14:11:54 +0200

    Remove unnecessary escape chars from email regex

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -28,7 +28,7 @@ export const objectMap = (object, mapFn) => {
 export function validateEmail(email) {
   // eslint-disable-next-line no-useless-escape
   var re =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
