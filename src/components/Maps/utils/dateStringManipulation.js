386f58b10731123e2ce1c93e4bc8ae5248fbb5a1
nmauersberg <n.mauersberg@gmail.com>
2021-10-25 20:03:28 +0200

    Refactoring and add tests for date utils

--- src/components/Maps/utils/dateStringManipulation.js
+++ src/components/Maps/utils/dateStringManipulation.js
@@ -1,23 +1,27 @@
+export const GER_LOCALE = 'de-DE';
+
 export const getGermanDateFormat = date => {
-  return new Date(date).toLocaleDateString('de-DE');
+  return new Date(date).toLocaleDateString(GER_LOCALE);
 };
 
 export const getDateWithWeekday = date => {
-  if (checkIfDateIsToday(date)) {
-    return checkIfDateIsToday(date);
-  } else if (checkIfDateIsTomorrow(date)) {
-    return checkIfDateIsTomorrow(date);
-  }
-  return `${getDayName(new Date(date))}, ${getDayWithMonth(date)}`;
+  const isToday = checkIfDateIsToday(date);
+  if (isToday) return 'Heute';
+
+  const isTomorrow = checkIfDateIsTomorrow(date);
+  if (isTomorrow) return 'Morgen';
+
+  const otherDay = `${getDayName(new Date(date))}, ${getDayWithMonth(date)}`;
+  return otherDay;
 };
 
-export const getDayName = (dateStr, locale = 'de-DE') => {
+export const getDayName = dateStr => {
   var date = new Date(dateStr);
-  return date.toLocaleDateString(locale, { weekday: 'long' });
+  return date.toLocaleDateString(GER_LOCALE, { weekday: 'long' });
 };
 
 export const localeTime = date => {
-  return new Date(date).toLocaleTimeString('de-DE', {
+  return new Date(date).toLocaleTimeString(GER_LOCALE, {
     hour: '2-digit',
     minute: '2-digit',
   });
@@ -27,21 +31,21 @@ export const getDayWithMonth = date => {
   return getGermanDateFormat(date).slice(0, 6);
 };
 
-const checkIfDateIsToday = date => {
+export const checkIfDateIsToday = date => {
   if (checkIfInRangeOfDaysX(date, 0)) {
-    return 'Heute';
+    return true;
   }
   return false;
 };
 
-const checkIfDateIsTomorrow = date => {
+export const checkIfDateIsTomorrow = date => {
   if (checkIfInRangeOfDaysX(date, 1)) {
-    return 'Morgen';
+    return true;
   }
   return false;
 };
 
-const checkIfInRangeOfDaysX = (dateStr, num) => {
+export const checkIfInRangeOfDaysX = (dateStr, num) => {
   const date = new Date(dateStr);
   const dateToCheck = new Date(
     date.getFullYear(),
