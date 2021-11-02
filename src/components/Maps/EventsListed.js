70873dfae9ed1022cc5389cd1b891e5f5bb875af
nmauersberg <n.mauersberg@gmail.com>
2021-11-02 10:45:38 +0100

    Events listed loading animation

--- src/components/Maps/EventsListed.js
+++ src/components/Maps/EventsListed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as s from './style.module.less';
 import * as dsm from './utils/dateStringManipulation';
+import { LoadingAnimation } from '../LoadingAnimation';
 
 export const EventsListed = ({ locationsFiltered }) => {
   let groupedEvents = {};
@@ -25,31 +26,37 @@ export const EventsListed = ({ locationsFiltered }) => {
 
   return (
     <>
-      <h2 className={s.moduleHeading}>Komm zu einer Sammelaktion</h2>
-      <div className={s.eventContainer}>
-        {Object.keys(groupedEvents).map(key => {
-          return (
-            <div key={key} className={s.eventDay}>
-              <h3 className={s.descriptionHeading}>
-                {dsm.getDateWithWeekday(groupedEvents[key][0].startTime)}
-              </h3>
-              {groupedEvents[key].map(event => {
-                return (
-                  <div className={s.eventDescription}>
-                    <p className={s.descriptionText}>{event.description}</p>
-                    <b>
-                      {dsm.localeTime(event.startTime)}-
-                      {dsm.localeTime(event.endTime)}
-                      {' Uhr, '}
-                      {event.address}
-                    </b>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
+      {Object.keys(groupedEvents).length > 0 ? (
+        <>
+          <h2 className={s.moduleHeading}>Komm zu einer Sammelaktion</h2>
+          <div className={s.eventContainer}>
+            {Object.keys(groupedEvents).map(key => {
+              return (
+                <div key={key} className={s.eventDay}>
+                  <h3 className={s.descriptionHeading}>
+                    {dsm.getDateWithWeekday(groupedEvents[key][0].startTime)}
+                  </h3>
+                  {groupedEvents[key].map(event => {
+                    return (
+                      <div className={s.eventDescription}>
+                        <p className={s.descriptionText}>{event.description}</p>
+                        <b>
+                          {dsm.localeTime(event.startTime)}-
+                          {dsm.localeTime(event.endTime)}
+                          {' Uhr, '}
+                          {event.address}
+                        </b>
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <LoadingAnimation />
+      )}
     </>
   );
 };
