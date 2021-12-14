af820d4a5adca6353f99b5decc31e1dd3e683103
nmauersberg <n.mauersberg@gmail.com>
2021-12-14 09:43:39 +0100

    Update banner text for christmas lottery

--- src/components/Layout/Header/StickyDonationBar/index.jsx
+++ src/components/Layout/Header/StickyDonationBar/index.jsx
@@ -1,24 +1,19 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import * as s from './style.module.less';
-import { MunicipalityContext } from '../../../../context/Municipality';
 import { StickyBannerContext } from '../../../../context/StickyBanner';
 import closeIcon from './close-icon.svg';
 
 export const StickyDonationBar = () => {
-  const { allMunicipalityStats } = useContext(MunicipalityContext);
   const { closeStickyBanner } = useContext(StickyBannerContext);
-
-  const qualifiedCount =
-    allMunicipalityStats?.qualifiedMunicipalities?.length || 0;
 
   return (
     <div className={s.donationBar}>
       <div className={s.donationBarItemContainer}>
         <p className={s.donationBarItem}>
-          Schon {qualifiedCount} Orte haben sich qualifiziert, ganz viele sind
-          auf dem Weg zum Ziel. Hilf mit weitere Orte ins Ziel zu bringen!
+          Weihnachtsverlosung mit Grundeinkommen! Klick hier, um zu spenden und
+          spannende Preise zu gewinnen
         </p>
-        {/* <button className={s.donationBarCTA}>Sei dabei</button> */}
+        <button className={s.donationBarCTA}>Zur Verlosung</button>
         <button className={s.closeButton} onClick={() => closeStickyBanner()}>
           <img
             className={s.closeIcon}
