1ff8ae79c44b6b17a4d3cff718a0089a62ea9628
Nicolai Mauersberg <74594947+nmauersberg@users.noreply.github.com>
2022-03-07 17:12:28 +0100

    Fix EventsListed - loading state (#382)

--- src/components/Maps/EventsListed.js
+++ src/components/Maps/EventsListed.js
@@ -1,27 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as s from './style.module.less';
 import * as dsm from './utils/dateStringManipulation';
 import { LoadingAnimation } from '../LoadingAnimation';
 
+/* The "EventsListed" Component should only display data,
+when there were events in the next days.
+
+Otherwise it renders nothing. */
+
 export const EventsListed = ({ locationsFiltered }) => {
-  let groupedEvents = {};
-  let locationsSorted = [];
+  const [loading, setLoading] = useState(false);
+  const [groupedEvents, setGroupedEvents] = useState({});
+
+  useEffect(() => {
+    if (locationsFiltered) {
+      setLoading(true);
+      const locationsSorted = locationsFiltered
+        ?.filter(location => location.startTime && location.endTime)
+        .sort(function (a, b) {
+          return new Date(a.startTime) - new Date(b.startTime);
+        });
+      const grouped = groupByDate(locationsSorted);
+      setGroupedEvents(grouped);
+      setLoading(false);
+    }
+  }, [locationsFiltered]);
 
   const groupByDate = locations => {
+    const grEvents = {};
     locations.forEach(location => {
       const eventDate = dsm.getGermanDateFormat(location.startTime);
-      if (!(eventDate in groupedEvents)) groupedEvents[eventDate] = [];
-      groupedEvents[eventDate].push(location);
+      if (!(eventDate in grEvents)) grEvents[eventDate] = [];
+      grEvents[eventDate].push(location);
     });
+    return grEvents;
   };
 
-  if (locationsFiltered) {
-    locationsSorted = locationsFiltered
-      ?.filter(location => location.startTime && location.endTime)
-      .sort(function (a, b) {
-        return new Date(a.startTime) - new Date(b.startTime);
-      });
-    groupByDate(locationsSorted);
+  if (loading) {
+    return <LoadingAnimation />;
   }
 
   return (
@@ -54,9 +70,7 @@ export const EventsListed = ({ locationsFiltered }) => {
             })}
           </div>
         </>
-      ) : (
-        <LoadingAnimation />
-      )}
+      ) : null}
     </>
   );
 };
