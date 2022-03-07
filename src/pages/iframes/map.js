4931972a1b0af965157fde7e9658480760bf7acc
nmauersberg <n.mauersberg@gmail.com>
2022-03-07 20:56:58 +0100

    Publish collection map for iframe

--- src/pages/iframes/map.js
+++ src/pages/iframes/map.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { CreateMeetup } from '../../components/Forms/Meetup';
 
-const TestFrame = () => {
+const IframeMap = () => {
   return (
-    <div>
-      <h2>Test</h2>
-    </div>
+    <CreateMeetup
+      mapConfig={{
+        state: 'berlin',
+        config: {
+          maxBounds: [
+            [13.05, 52.2],
+            [13.8, 52.8],
+          ],
+        },
+      }}
+    />
   );
 };
 
-export default TestFrame;
+export default IframeMap;
