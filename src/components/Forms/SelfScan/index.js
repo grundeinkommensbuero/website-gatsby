2979d560d318733caf5b8f74c09ffdd6dd289d73
Valioesi <Vali_Schagerl@web.de>
2022-05-09 17:54:18 +0200

    Hint for default list

--- src/components/Forms/SelfScan/index.js
+++ src/components/Forms/SelfScan/index.js
@@ -310,6 +310,9 @@ const CountSignaturesForm = ({
                           className={s.label}
                           inputClassName={s.listIdField}
                         ></Field>
+                        <p className={s.idHint}>
+                          Wenn die Liste keinen Barcode hat, gib bitte 0 ein.
+                        </p>
                       </div>
                     )}
                   </FormSection>
